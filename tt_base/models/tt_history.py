b4c75635669abc464e7cbe903e9a03b85e43d657
Joshua Ginove Hertanto Wijaya <joshuaghw@yahoo.com>
2019-06-14 07:35:09 +0000

    Revert "Merge branch 'tour_pricelist' into 'master'"
    This reverts merge request !5

--- tt_base/models/tt_history.py
+++ tt_base/models/tt_history.py
@@ -1,5 +1,43 @@
-from odoo import models, api, _
+from odoo import models, api, fields, _
 import html
+
+
+class SkippedKeys(models.Model):
+    _name = 'tt.skipped.keys'
+
+    model_name = fields.Char('Model Name')
+    field_name = fields.Char('Field Name')
+    active = fields.Boolean('Active', default=True)
+
+    SKIPPED_KEYS = {}
+
+    @api.model
+    def create(self, vals_list):
+        new_record = super(SkippedKeys, self).create(vals_list)
+        self._register_hook()
+        return  new_record
+
+    @api.model
+    def write(self, vals):
+        super(SkippedKeys, self).write(vals)
+        self._register_hook()
+
+    @api.multi
+    def unlink(self):
+        deleted_record = super(SkippedKeys, self).unlink()
+        self._register_hook()
+        return deleted_record
+
+    def fill_skipped_keys(self):
+        self.SKIPPED_KEYS.clear()
+        for rec in self.search([]):
+            if not rec.model_name in self.SKIPPED_KEYS:
+                self.SKIPPED_KEYS[rec.model_name] = []
+            self.SKIPPED_KEYS[rec.model_name].append(rec.field_name)
+
+    def _register_hook(self):
+        self.fill_skipped_keys()
+        print(self.SKIPPED_KEYS)
 
 
 class TtHistory(models.Model):
@@ -10,6 +48,10 @@ class TtHistory(models.Model):
     def write(self, value):
         self_dict = self.read()
         key_list = [key for key in value.keys()]
+
+        #tempat hardcode skip key
+        skipped_keys = self.env['tt.skipped.keys'].SKIPPED_KEYS
+
         for key in key_list:
             #skipped key
             if self._name in skipped_keys:
@@ -35,59 +77,38 @@ class TtHistory(models.Model):
 
                 # Non-Relation Field
                 else:
-                    # start : kode ini masih beresiko (hard coded). dapat diubah kapan saja
-                    if self.fields_get().get(key)['type'] == 'many2one':
-                        old_value = self_dict[0].get(key)
-                        # print(old_value)
-                        if old_value is False:
-                            old_value = 'None'
-                        else:
-                            old_value = self_dict[0].get(key)[1]
-                        new_val = self.env[self.fields_get().get(key)['relation']].search([('id', 'in', [value[key]])]).name_get()
-                        if not new_val:
-                            if str(self.env[self.fields_get().get(key)['relation']]) == 'res.users()':  # masih rawan
-                                new_value = self.env.user.name
-                            else:
-                                new_value = old_value  # sementara
-                        else:
-                            new_value = self.env[self.fields_get().get(key)['relation']] \
-                                        .search([('id', 'in', [value[key]])]).name_get()[0][1]
-                    # end
+                    old_value = self_dict[0].get(key)
+                    new_value = value[key]
+                # print(key + ' : ' + str(new_value))
 
-                    # Non-Relation Field
-                    else:
-                        old_value = self_dict[0].get(key)
-                        new_value = value[key]
-                    # print(key + ' : ' + str(new_value))
+                # Print History
+                if self.fields_get().get(key)['type'] == 'binary':
+                    self.message_post(body=_("%s has been changed.") %
+                                           (self.fields_get().get(key)['string']))  # Model String / Label
+                elif self.fields_get().get(key)['type'] == 'html':
+                    # Un-escape HTML
+                    old_value = html.unescape(old_value)
+                    new_value = html.unescape(new_value)
 
-                    # Print History
-                    if self.fields_get().get(key)['type'] == 'binary':
-                        self.message_post(body=_("%s has been changed.") %
-                                               (self.fields_get().get(key)['string']))  # Model String / Label
-                    elif self.fields_get().get(key)['type'] == 'html':
-                        # Un-escape HTML
-                        old_value = html.unescape(old_value)
-                        new_value = html.unescape(new_value)
+                    # " to '
+                    old_value = old_value.replace('"', '\'')
+                    new_value = new_value.replace('"', '\'')
 
-                        # " to '
-                        old_value = old_value.replace('"', '\'')
-                        new_value = new_value.replace('"', '\'')
-
-                        if old_value != new_value:
-                            # print('test')
-                            self.message_post(body=_("%s has been changed from %s to %s by %s.") %
-                                                    (self.fields_get().get(key)['string'],  # Model String / Label
-                                                     old_value,  # Old Value
-                                                     # value[key],  # New Value
-                                                     new_value,  # new value
-                                                     self.env.user.name))  # User that Changed the Value
-                    else:
+                    if old_value != new_value:
+                        # print('test')
                         self.message_post(body=_("%s has been changed from %s to %s by %s.") %
-                                               (self.fields_get().get(key)['string'],  # Model String / Label
-                                                old_value,  # Old Value
-                                                # value[key],  # New Value
-                                                new_value,  # new value
-                                                self.env.user.name))  # User that Changed the Value
+                                                (self.fields_get().get(key)['string'],  # Model String / Label
+                                                 old_value,  # Old Value
+                                                 # value[key],  # New Value
+                                                 new_value,  # new value
+                                                 self.env.user.name))  # User that Changed the Value
+                else:
+                    self.message_post(body=_("%s has been changed from %s to %s by %s.") %
+                                           (self.fields_get().get(key)['string'],  # Model String / Label
+                                            old_value,  # Old Value
+                                            # value[key],  # New Value
+                                            new_value,  # new value
+                                            self.env.user.name))  # User that Changed the Value
         return super(TtHistory, self).write(value)
 
 # For Debug Purposes
