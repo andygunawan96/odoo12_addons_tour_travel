c4dd85793bb0ad726c6b929e8e104e1920987bcb
rodex-it-04 <it@rodextravel.tours>
2019-07-04 10:59:19 +0700

    melengkapi pricing_provider

--- tt_engine_pricing/models/pricing_provider.py
+++ tt_engine_pricing/models/pricing_provider.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api, _
+from ...tools import variables
 
 
 class PricingProvider(models.Model):
@@ -12,9 +13,10 @@ class PricingProvider(models.Model):
         ('sale', 'Sale'),
         ('commission', 'Commission'),
     ], 'Pricing Type', required=True)
-    carrier_ids = fields.Many2many('tt.transport.carrier', 'tt_carrier_pricing_rel', 'pricing_id', 'carrier_id',
+    carrier_ids = fields.Many2many('tt.transport.carrier', 'tt_pricing_carrier_rel', 'pricing_id', 'carrier_id',
                                    string='Carriers')
     is_all_carrier = fields.Boolean('Is All Carrier', default=False)
+    line_ids = fields.One2many('tt.pricing.provider.line', 'pricing_id', 'Configs')
     active = fields.Boolean('Active', default=True)
 
 
@@ -22,16 +24,38 @@ class PricingProviderLine(models.Model):
     _name = 'tt.pricing.provider.line'
 
     name = fields.Char('Name', requried=True)
-    sequence = fields.Integer('Sequence')
+    sequence = fields.Integer('Sequence', default=50, required=True)
+    pricing_id = fields.Many2one('tt.pricing.provider', 'Pricing Provider')
     date_from = fields.Datetime('Date From', required=True)
     date_to = fields.Datetime('Date To', required=True)
-    # origin_type =
-    # origin_ids = fields.Many2many('tt.destinations')
-    # origin_country_ids = fields.Many2many()
-    # destination_type
-    # destination_ids
-    # destination_country_ids
-    # amount_per_route
-    # amount_per_segment
-    # amount_per_pax
+    origin_type = fields.Selection(variables.ACCESS_TYPE, 'Origin Type')
+    origin_ids = fields.Many2many('tt.destinations', 'tt_pricing_line_origin_rel', 'pricing_line_id', 'origin_id',
+                                  string='Origins')
+    origin_city_ids = fields.Many2many('res.city', 'tt_pricing_line_origin_city_rel', 'pricing_line_id', 'city_id',
+                                       string='Origin Cities')
+    origin_country_ids = fields.Many2many('res.country', 'tt_pricing_line_origin_country_rel', 'pricing_line_id', 'country_id',
+                                          string='Origin Countries')
+    destination_type = fields.Selection(variables.ACCESS_TYPE, 'Destination Type')
+    destination_ids = fields.Many2many('tt.destinations', 'tt_pricing_line_destination_rel', 'pricing_line_id', 'destination_id',
+                                       string='Destinations')
+    destination_city_ids = fields.Many2many('res.city', 'tt_pricing_line_destination_city_rel', 'pricing_line_id', 'city_id',
+                                            string='Destination Cities')
+    destination_country_ids = fields.Many2many('res.country', 'tt_pricing_line_destination_country_rel', 'pricing_line_id', 'country_id',
+                                               string='Destination Countries')
+    currency_id = fields.Many2one('res.currency', 'Currency')
+    amount_per_route = fields.Float('Ammount per Route')
+    amount_per_segment = fields.Float('Amount per Segment')
+    amount_per_pax = fields.Float('Amount per Pax')
+    basic_amount_type = fields.Selection(variables.AMOUNT_TYPE, 'Basic Amount Type', default='percentage')
+    basic_amount = fields.Float('Basic Amount', default=0)
+    tax_amount_type = fields.Selection(variables.AMOUNT_TYPE, 'Tax Amount Type', default='percentage')
+    tax_amount = fields.Float('Tax Amount', default=0)
+    after_tax_amount_type = fields.Selection(variables.AMOUNT_TYPE, 'After Tax Amount Type', default='percentage')
+    after_tax_amount = fields.Float('After Tax Amount', default=0)
+    minimum_limit = fields.Monetary('Minimum Limit', default=0)
+    minimum_amount_type = fields.Selection(variables.AMOUNT_TYPE, 'Minimum Amount Type', default='amount')
+    minimum_amount = fields.Monetary('Minimum Amount', default=0)
+    upper_limit = fields.Float('Upper Limit', default=0)
+    upper_amount_type = fields.Selection(variables.AMOUNT_TYPE, 'Upper Amount Type', default='percentage')
+    upper_amount = fields.Float('Upper Ammount', default=0)
     active = fields.Boolean('Active', default=True)
