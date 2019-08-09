4764f47da37d5c2981509fffb74b8f2926ee34a6
rodex-it-04 <it@rodextravel.tours>
2019-08-09 13:44:02 +0700

    membuat model tt_ssr

--- tt_base/models/__init__.py
+++ tt_base/models/__init__.py
@@ -27,3 +27,4 @@ from . import tt_provider_type
 from . import tt_customer_parent
 from . import tt_customer_parent_type
 from . import error_api
+from . import tt_ssr
--- tt_base/models/tt_ssr.py
+++ tt_base/models/tt_ssr.py
@@ -23,3 +23,29 @@ class TtSSRList(models.Model):
     name = fields.Char('Name', required=True)
     code = fields.Char('Code', required=True)
     description = fields.Text('Description', default='')
+    category_id = fields.Many2one('tt.ssr.category', 'Category')
+    provider_id = fields.Many2one('tt.provider', required=True)
+
+    def to_dict(self):
+        res = {
+            'name': self.name,
+            'code': self.code,
+            'description': self.description and self.description or '',
+            'category_id': self.category_id and self.category_id.to_dict() or {},
+            'provider_id': self.provider_id and self.provider_id.to_dict() or {}
+        }
+        return res
+
+
+class TtProviderSSRListInherit(models.Model):
+    _inherit = 'tt.provider'
+
+    ssr_ids = fields.One2many('tt.ssr.list', 'provider_id', 'SSRs')
+
+    def to_dict(self):
+        ssr_ids = [rec.to_dict() for rec in self.ssr_ids]
+        res = super(TtProviderSSRListInherit, self).to_dict()
+        res.update({
+            'ssr_ids': ssr_ids
+        })
+        return res
