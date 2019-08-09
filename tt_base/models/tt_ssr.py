c03220447b8d03c11c967240271a24a4c9d852db
rodex-it-04 <it@rodextravel.tours>
2019-08-09 14:54:01 +0700

    menambahkan lines pada ssr list

--- tt_base/models/tt_ssr.py
+++ tt_base/models/tt_ssr.py
@@ -25,15 +25,37 @@ class TtSSRList(models.Model):
     description = fields.Text('Description', default='')
     category_id = fields.Many2one('tt.ssr.category', 'Category')
     provider_id = fields.Many2one('tt.provider', required=True)
+    line_ids = fields.One2many('tt.ssr.list.line', 'ssr_id', 'Lines')
+    active = fields.Boolean('Active', default=True)
+
+    def to_dict(self):
+        line_ids = [rec.to_dict() for rec in self.line_ids]
+        res = {
+            'name': self.name,
+            'code': self.code,
+            'description': self.description and self.description or '',
+            'category_id': self.category_id and self.category_id.to_dict() or {},
+            'provider_id': self.provider_id and self.provider_id.to_dict() or {},
+            'line_ids': line_ids
+        }
+        return res
+
+
+class TtSSRListLine(models.Model):
+    _name = 'tt.ssr.list.line'
+
+    name = fields.Char('Name', required=True)
+    code = fields.Char('Code', required=True)
+    value = fields.Char('Value', default='')
+    ssr_id = fields.Many2one('tt.ssr.list', 'SSR', readonly=1)
     active = fields.Boolean('Active', default=True)
 
     def to_dict(self):
         res = {
             'name': self.name,
             'code': self.code,
-            'description': self.description and self.description or '',
-            'category_id': self.category_id and self.category_id.to_dict() or {},
-            'provider_id': self.provider_id and self.provider_id.to_dict() or {}
+            'value': self.value,
+            'ssr_id': self.ssr_id and self.ssr_id.to_dict() or {}
         }
         return res
 
