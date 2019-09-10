ec719c9b26b41022ed240d4ddc231b586acca30e
Andy Gunawan <it@rodextravel.tours>
2019-09-10 15:48:22 +0700

    andy_10_sept_backend

--- tt_reservation_activity/models/__init__.py
+++ tt_reservation_activity/models/__init__.py
@@ -1,6 +1,9 @@
+from . import tt_service_charge_activity
+from . import tt_provider_activity
 from . import tt_master_activity
 from . import tt_activity_category
 from . import tt_master_activity_lines
 from . import tt_activity_booking_option
 from . import tt_reservation_activity
-from . import tt_reservation_activity_line
+from . import tt_reservation_passenger_activity
+
