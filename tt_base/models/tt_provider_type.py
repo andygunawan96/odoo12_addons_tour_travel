315850f6a0038fa37eaaf2122ecf0cd6e615e889
samvi <it@rodextravel.tours>
2019-10-01 11:57:11 +0700

    menambahkan model tt transport carrier pada gateway dan fungsi sync dengan backend

--- tt_base/models/tt_provider_type.py
+++ tt_base/models/tt_provider_type.py
@@ -1,5 +1,10 @@
 from odoo import models, fields, api
 from ...tools import variables
+from ...tools.api import Response
+import logging, traceback
+
+
+_logger = logging.getLogger(__name__)
 
 
 class ProviderType(models.Model):
@@ -25,4 +30,14 @@ class ProviderType(models.Model):
             'name': self.name,
             'code': self.code,
         }
+        return res
+
+    def get_provider_type_list_api(self, data, context):
+        try:
+            _objs = self.sudo().search([])
+            response = [rec.to_dict() for rec in _objs]
+            res = Response().get_no_error(response)
+        except Exception as e:
+            _logger.error('Error Get Provider Type List, %s, %s' % (str(e), traceback.format_exc()))
+            res = Response().get_error(str(e), 500)
         return res
