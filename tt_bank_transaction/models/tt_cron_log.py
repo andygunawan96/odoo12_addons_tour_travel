77dd6e287694090be5d89a6f5303fedc92b1d0b8
edo <bella@rodextravel.tours>
2019-12-06 15:44:28 +0700

    bank transaction with date

--- tt_bank_transaction/models/tt_cron_log.py
+++ tt_bank_transaction/models/tt_cron_log.py
@@ -1,7 +1,7 @@
 from odoo import api,models,fields
 from ...tools import variables
 import logging,traceback
-from datetime import datetime
+from datetime import datetime, timedelta
 _logger = logging.getLogger(__name__)
 
 class ttCronTopUpValidator(models.Model):
@@ -46,8 +46,8 @@ class ttCronTopUpValidator(models.Model):
             data = {
                 'account_number': 5110150000,
                 'provider': 'bca',
-                'startdate': datetime.today().strftime("%Y-%m-%d"),
-                'enddate': datetime.today().strftime("%Y-%m-%d"),
+                'startdate': (datetime.today() + timedelta(hours=7)).strftime("%Y-%m-%d"),
+                'enddate': (datetime.today() + timedelta(hours=7)).strftime("%Y-%m-%d"),
             }
                 #called function to proceed data and input in bank transaction
             result = self.env['tt.bank.transaction'].get_data(data)
@@ -55,3 +55,12 @@ class ttCronTopUpValidator(models.Model):
         except Exception as e:
             self.create_cron_log_folder()
             self.write_cron_log('auto get bank transaction')
+
+    def cron_auto_proceed_bank_transaction(self):
+        try:
+            bank_transaction_data = self.env['tt.bank.transaction'].search([('transaction_date', '<', datetime.today()), ('transaction_process', '=', 'unprocess')])
+            for i in bank_transaction_data:
+                i.change_process_status()
+        except Exception as e:
+            self.create_cron_log_folder()
+            self.write_cron_log("auto proceed yesterday transaction data")
