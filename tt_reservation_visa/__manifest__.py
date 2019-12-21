a0315cac679237d598b4d21936eb5d2aee76dd13
andre <setiawanandre06@gmail.com>
2019-12-21 15:30:46 +0700

    Agent invoice offline: - set state invoice = confirm
    Agent invoice visa:
    - set state invoice = confirm
    - Fix acquirer id
    
    HO Invoice:
    - add tour & visa
    
    Offline :
    - Fix pax agent offline
    - Fix action cancel
    
    Visa :
    - Update get booking

--- tt_reservation_visa/__manifest__.py
+++ tt_reservation_visa/__manifest__.py
@@ -1,22 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': "tt_reservation_visa",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
+    'version' : 'beta',
+    'summary': """Visa Document""",
+    'sequence': 2,
     'description': """
-        Long description of module's purpose
+        TT RESERVATION VISA
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'category': 'booking',
+    'website': "http://www.rodextrip.com",
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -49,4 +41,7 @@
     ],
     # only loaded in demonstration mode
     'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
