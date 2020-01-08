a317ed3cbfbea71777adcf64663ef9e0dee7b191
Andy Gunawan <it@rodextravel.tours>
2020-01-08 15:57:47 +0700

    reschedule_part2

--- tt_reschedule/models/tt_seat_reschedule.py
+++ tt_reschedule/models/tt_seat_reschedule.py
@@ -6,3 +6,18 @@ class TtSeatReschedule(models.Model):
     _inherit = 'tt.seat.airline'
 
     segment_id = fields.Many2one('tt.segment.reschedule', 'Segment')
+
+    def get_seat_passenger_domain(self):
+        pax_id_list = []
+        for rec in self.segment_id.passenger_ids:
+            pax_id_list.append(rec.id)
+        return [('id', 'in', pax_id_list)]
+
+    passenger_id = fields.Many2one('tt.reservation.passenger.airline', 'Passenger', domain=get_seat_passenger_domain)
+
+    @api.depends('segment_id')
+    @api.onchange('segment_id')
+    def _onchange_domain_passenger(self):
+        return {'domain': {
+            'passenger_id': self.get_seat_passenger_domain()
+        }}
