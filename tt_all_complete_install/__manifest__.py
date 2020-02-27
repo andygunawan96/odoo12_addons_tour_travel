1a3ea45b9c0431f750ef3d61ac1f496cb66b0997
ivan <ivan@rodextravel.tours>
2020-02-27 06:47:42 +0000

    Update tt all complete install

--- tt_all_complete_install/__manifest__.py
+++ tt_all_complete_install/__manifest__.py
@@ -20,7 +20,22 @@
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'base_setup',
-
+                'tt_api_management',
+                'tt_reschedule',
+                'tt_merge_record',
+                'tt_report_selling',
+                'tt_public_holiday',
+                'tt_frontend_banner', 'tt_bank_transaction',
+                'tt_billing_statement', 'tt_agent_report_billing',
+                'tt_reservation_report_airline',
+                'tt_agent_report_invoice', 'tt_agent_report_offline',
+                'tt_agent_report_recap_reservation', 'tt_agent_report_recap_transaction',
+                'tt_agent_sales_activity',
+                # 'tt_agent_sales_agent_registration',
+                'tt_agent_sales_airline', 'tt_agent_sales_hotel', 'tt_agent_sales_offline',
+                'tt_agent_sales_passport', 'tt_agent_sales_tour',
+                'tt_agent_sales_train', 'tt_agent_sales_visa', 'web_m2x_options',
+                'tt_agent_report_balance', 'tt_agent_report_passport', 'tt_agent_report_visa'
     ],
 
     # always loaded
