6a3fde5f1376b9e0b3a239c2a9bee6273745a53b
Andy Gunawan <it@rodextravel.tours>
2020-04-06 11:57:57 +0700

    book_email_all_products

--- tt_reservation_tour/models/tt_reservation_tour.py
+++ tt_reservation_tour/models/tt_reservation_tour.py
@@ -118,15 +118,20 @@ class ReservationTour(models.Model):
 
             try:
                 if self.agent_type_id.is_send_email_issued:
-                    temp_data = {
-                        'provider_type': 'tour',
-                        'order_number': self.name,
-                        'type': 'issued',
-                    }
-                    temp_context = {
-                        'co_agent_id': self.agent_id.id
-                    }
-                    self.env['tt.email.queue'].create_email_queue(temp_data, temp_context)
+                    mail_created = self.env['tt.email.queue'].sudo().search([('res_id', '=', self.id), ('res_model', '=', self._name), ('type', '=', 'issued_tour')], limit=1)
+                    if not mail_created:
+                        temp_data = {
+                            'provider_type': 'tour',
+                            'order_number': self.name,
+                            'type': 'issued',
+                        }
+                        temp_context = {
+                            'co_agent_id': self.agent_id.id
+                        }
+                        self.env['tt.email.queue'].create_email_queue(temp_data, temp_context)
+                    else:
+                        _logger.info('Issued email for {} is already created!'.format(self.name))
+                        raise Exception('Issued email for {} is already created!'.format(self.name))
             except Exception as e:
                 _logger.info('Error Create Email Queue')
 
@@ -264,6 +269,25 @@ class ReservationTour(models.Model):
                     'state': temp_state
                 })
             self.tour_id.sudo().write(write_vals)
+
+            try:
+                if self.agent_type_id.is_send_email_booked:
+                    mail_created = self.env['tt.email.queue'].sudo().search([('res_id', '=', self.id), ('res_model', '=', self._name), ('type', '=', 'booked_tour')], limit=1)
+                    if not mail_created:
+                        temp_data = {
+                            'provider_type': 'tour',
+                            'order_number': self.name,
+                            'type': 'booked',
+                        }
+                        temp_context = {
+                            'co_agent_id': self.agent_id.id
+                        }
+                        self.env['tt.email.queue'].create_email_queue(temp_data, temp_context)
+                    else:
+                        _logger.info('Booking email for {} is already created!'.format(self.name))
+                        raise Exception('Booking email for {} is already created!'.format(self.name))
+            except Exception as e:
+                _logger.info('Error Create Email Queue')
 
     def action_cancel_by_manager(self):
         self.action_refund()
