e876b251d43b0447dbc02b0834c53c92086be2de
andre <setiawanandre06@gmail.com>
2020-05-26 16:11:09 +0700

    Reconcile : - Buat printout excel reconcile - Buat template tools excel untuk font color blue & green

--- tt_reservation/models/tt_reconcile_transaction.py
+++ tt_reservation/models/tt_reconcile_transaction.py
@@ -1,6 +1,10 @@
 from odoo import api,fields,models
 from datetime import datetime
 from odoo.exceptions import UserError
+from io import BytesIO
+import xlsxwriter
+import base64
+from ...tools import tools_excel
 
 class TtReconcileTransaction(models.Model):
     _name = 'tt.reconcile.transaction'
@@ -14,6 +18,7 @@ class TtReconcileTransaction(models.Model):
     reconcile_lines_ids = fields.One2many('tt.reconcile.transaction.lines','reconcile_transaction_id','Line(s)')
     transaction_date = fields.Date('Transaction Date', readonly=True)
     state = fields.Selection([('open','Open')],'State', default='open', readonly=True)
+    excel_file = fields.Binary('Excel File')
 
     @api.depends('provider_id','transaction_date')
     def _compute_display_reconcile_name(self):
@@ -47,6 +52,16 @@ class TtReconcileTransaction(models.Model):
             'domain': [('reconcile_transaction_id', '=', self.id)],
             'target': 'current',
         }
+
+    def print_report_excel(self):
+        datas = {'id': self.id}
+        res = self.read()
+        res = res and res[0] or {}
+        datas['form'] = res
+        url = self.env['tt.report.printout.reconcile'].print_report_excel(datas)
+        self.excel_file = base64.encodebytes(url['value'])
+        return url
+
 
 class TtReconcileTransactionLines(models.Model):
     _name = 'tt.reconcile.transaction.lines'
@@ -89,3 +104,225 @@ class TtReconcileTransactionLines(models.Model):
             self.state = 'not_match'
         else:
             raise UserError('Can only unignore [ignore] state.')
+
+
+class PrintoutReconcile(models.AbstractModel):
+    _name = 'tt.report.printout.reconcile'
+    _description = 'Rodex Model'
+
+    @staticmethod
+    def _select():
+        return """
+        rt.ticket_numbers, rt.agent_name, rt.pnr, rt.transaction_code, rt.type, rt.booking_time, rt.issued_time, rt.state, rt.base_price,
+        rt.tax, rt.commission, rt.vendor_start_balance, rt.vendor_end_balance, rt.total, rt.description
+        """
+
+    @staticmethod
+    def _from():
+        return """
+        tt_reconcile_transaction_lines rt
+        """
+
+    @staticmethod
+    def _where(reconcile_id):
+        where = "rt.reconcile_transaction_id = " + str(reconcile_id)
+        return where
+
+    @staticmethod
+    def _group_by():
+        return """
+        rt.id
+        """
+
+    @staticmethod
+    def _order_by():
+        return """
+        rt.id
+        """
+
+    def _lines(self, data):
+        query = 'SELECT ' + self._select() + \
+                'FROM ' + self._from() + \
+                'WHERE ' + self._where(data['id']) + \
+                'ORDER BY ' + self._order_by()
+        self.env.cr.execute(query)
+        return self.env.cr.dictfetchall()
+
+    @staticmethod
+    def ticket_to_list(ticket):
+        if ticket:
+            ticket_list = ticket.split('\n')
+            # Remove empty string
+            while '' in ticket_list:
+                ticket_list.remove('')
+            return ticket_list
+        else:
+            ticket = ['']
+            return ticket
+
+    def prepare_values(self, data):
+        lines = self._lines(data)
+        for line in lines:
+            line['ticket_numbers'] = self.ticket_to_list(line['ticket_numbers'])
+        data['subtitle'] = 'Reconcile Report: '
+
+        return {
+            'lines': lines,
+            'data': data
+        }
+
+    def print_report_excel(self, data):
+        values = self.prepare_values(data)
+
+        stream = BytesIO()
+        workbook = xlsxwriter.Workbook(stream)  # create a new workbook constructor
+        style = tools_excel.XlsxwriterStyle(workbook)  # set excel style
+        row_height = 13
+
+        sheet_name = 'Reconcile Report'  # get subtitle
+        sheet = workbook.add_worksheet(sheet_name)  # add a new worksheet to workbook
+        sheet.set_landscape()
+        sheet.hide_gridlines(2)  # Hide screen and printed gridlines.
+
+        # ======= TITLE & SUBTITLE ============
+        sheet.merge_range('A1:L2', 'Reconcile Report', style.title)  # set merge cells for agent name
+        sheet.merge_range('A3:L4', values['data']['form']['display_reconcile_name'], style.title2)  # set merge cells for title
+        sheet.freeze_panes(9, 0)  # freeze panes mulai dari row 1-10
+
+        # ======= TABLE HEAD ==========
+        sheet.write('A9', 'Ticket Number', style.table_head_center)
+        sheet.write('B9', 'PNR', style.table_head_center)
+        sheet.write('C9', 'Agent Name', style.table_head_center)
+        sheet.write('D9', 'Transaction Code', style.table_head_center)
+        sheet.write('E9', 'Type', style.table_head_center)
+        sheet.write('F9', 'Booking Time', style.table_head_center)
+        sheet.write('G9', 'Issued Time', style.table_head_center)
+        sheet.write('H9', 'Base Price', style.table_head_center)
+        sheet.write('I9', 'Tax', style.table_head_center)
+        sheet.write('J9', 'Commission', style.table_head_center)
+        sheet.write('K9', 'Total Price', style.table_head_center)
+        sheet.write('L9', 'Vendor Start Balance', style.table_head_center)
+        sheet.write('M9', 'Vendor End Balance', style.table_head_center)
+        sheet.write('N9', 'State', style.table_head_center)
+        sheet.write('O9', 'Description', style.table_head_center)
+
+        # ====== SET WIDTH AND HEIGHT ==========
+        sheet.set_row(0, row_height)  # set_row(row, height) -> row 0-4 (1-5)
+        sheet.set_row(1, row_height)
+        sheet.set_row(2, row_height)
+        sheet.set_row(3, row_height)
+        sheet.set_row(4, row_height)
+        sheet.set_row(8, 30)
+        sheet.set_column('A:A', 5)  # set_column(first_col, last_col, width)
+        sheet.set_column('B:A', 13)
+        sheet.set_column('C:C', 19)
+        sheet.set_column('D:D', 15)
+        sheet.set_column('E:E', 10)
+        sheet.set_column('F:F', 18)
+        sheet.set_column('G:G', 18)
+        sheet.set_column('H:H', 17)
+        sheet.set_column('I:I', 17)
+        sheet.set_column('J:J', 17)
+        sheet.set_column('K:K', 17)
+        sheet.set_column('L:L', 20)
+        sheet.set_column('M:M', 20)
+        sheet.set_column('N:N', 17)
+        sheet.set_column('O:O', 30)
+
+        row_data = 8
+        for rec in values['lines']:
+            if rec['ticket_numbers']:
+                for ticket in rec['ticket_numbers']:
+                    # Style
+                    row_data += 1
+
+                    sty_table_data_center = style.table_data_center
+                    sty_table_data = style.table_data
+                    sty_datetime = style.table_data_datetime
+                    sty_date = style.table_data_date
+                    sty_amount = style.table_data_amount
+
+                    if rec['state'] == 'match':
+                        sty_table_data_center = style.table_data_center_green
+                        sty_table_data = style.table_data_green
+                        sty_datetime = style.table_data_datetime_green
+                        sty_date = style.table_data_datetime_green
+                        sty_amount = style.table_data_amount_green
+                    elif rec['state'] == 'done':
+                        sty_table_data_center = style.table_data_center_blue
+                        sty_table_data = style.table_data_blue
+                        sty_datetime = style.table_data_datetime_blue
+                        sty_date = style.table_data_datetime_blue
+                        sty_amount = style.table_data_amount_blue
+                    elif rec['state'] == 'ignore':
+                        sty_table_data_center = style.table_data_center_red
+                        sty_table_data = style.table_data_red
+                        sty_datetime = style.table_data_datetime_red
+                        sty_date = style.table_data_datetime_red
+                        sty_amount = style.table_data_amount_red
+
+                    if row_data % 2 == 0:  # row genap : bg abu2
+                        sty_table_data_center = style.table_data_center_even
+                        sty_table_data = style.table_data_even
+                        sty_datetime = style.table_data_datetime_even
+                        sty_date = style.table_data_date_even
+                        sty_amount = style.table_data_amount_even
+
+                        if rec['state'] == 'match':
+                            sty_table_data_center = style.table_data_center_green_even
+                            sty_table_data = style.table_data_green_even
+                            sty_datetime = style.table_data_datetime_green_even
+                            sty_date = style.table_data_datetime_green_even
+                            sty_amount = style.table_data_amount_green_even
+                        elif rec['state'] == 'done':
+                            sty_table_data_center = style.table_data_center_blue_even
+                            sty_table_data = style.table_data_blue_even
+                            sty_datetime = style.table_data_datetime_blue_even
+                            sty_date = style.table_data_datetime_blue_even
+                            sty_amount = style.table_data_amount_blue_even
+                        elif rec['state'] == 'ignore':
+                            sty_table_data_center = style.table_data_center_red_even
+                            sty_table_data = style.table_data_red_even
+                            sty_datetime = style.table_data_datetime_red_even
+                            sty_date = style.table_data_datetime_red_even
+                            sty_amount = style.table_data_amount_red_even
+
+                    sty_table_data_center.font_size = 10
+                    sty_table_data.font_size = 10
+                    sty_datetime.font_size = 10
+                    sty_date.font_size = 10
+                    sty_amount.font_size = 10
+
+                    # Content
+                    sheet.write(row_data, 0, ticket, sty_table_data)
+                    sheet.write(row_data, 1, rec['pnr'], sty_table_data)
+                    sheet.write(row_data, 2, rec['agent_name'], sty_table_data)
+                    sheet.write(row_data, 3, rec['transaction_code'], sty_table_data)
+                    sheet.write(row_data, 4, rec['type'], sty_table_data)
+                    sheet.write(row_data, 5,
+                                datetime.strptime(str(rec['booking_time']), "%Y-%m-%d %H:%M:%S").strftime(
+                                    "%Y-%m-%d %H:%M:%S") if rec[
+                                    'booking_time'] else '',
+                                sty_datetime)
+                    sheet.write(row_data, 6,
+                                datetime.strptime(str(rec['issued_time']), "%Y-%m-%d %H:%M:%S").strftime(
+                                    "%Y-%m-%d %H:%M:%S") if rec[
+                                    'issued_time'] else '',
+                                sty_datetime)
+                    sheet.write(row_data, 7, rec['base_price'], sty_amount)
+                    sheet.write(row_data, 8, rec['tax'], sty_amount)
+                    sheet.write(row_data, 9, rec['commission'], sty_amount)
+                    sheet.write(row_data, 10, rec['total'], sty_amount)
+                    sheet.write(row_data, 11, rec['vendor_start_balance'], sty_amount)
+                    sheet.write(row_data, 12, rec['vendor_end_balance'], sty_amount)
+                    sheet.write(row_data, 13, rec['state'], sty_table_data)
+                    sheet.write(row_data, 14, rec['description'], sty_table_data)
+
+        workbook.close()
+
+        return {
+            'type': 'ir.actions.act_url',
+            'url': 'web/content/?model=tt.reconcile.transaction&field=excel_file&download=true&id=%s&filename=reconcile_report.xlsx' % (data['id']),
+            'target': 'new',
+            'value': stream.getvalue()
+        }
