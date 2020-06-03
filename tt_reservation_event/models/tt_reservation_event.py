04b0bfc0b25d6ca309f739a08a22a39c3e825b44
vin <vincent@rodextravel.tours>
2020-06-03 08:22:01 +0700

    report andre, update default + domain untuk provider di event

--- tt_reservation_event/models/tt_reservation_event.py
+++ tt_reservation_event/models/tt_reservation_event.py
@@ -221,6 +221,7 @@ class ReservationEvent(models.Model):
             event_option_codes = req.get('event_option_codes')
             provider = req.get('provider')
             event_answer = req.get('event_answer', [])
+            special_request = req.get('special_request', 'No Special Request')
 
             #create all dependencies
             booker_obj = self.create_booker_api(booker_data, context)
@@ -274,9 +275,9 @@ class ReservationEvent(models.Model):
                 'booking_id': book_obj.id,
                 'balance_due': 0,  # di PNR
                 'event_id': event_id and event_id.id or False,
-                'event_product_id': event_id.id,
-                'event_product': event_id and event_id.name or req.get('event_name'),
-                'event_product_uuid': event_id and event_id.uuid or req.get('event_id'),
+                'event_product_id': opt_obj.id,
+                'event_product': opt_obj and opt_obj.grade or req.get('event_name'),
+                'event_product_uuid': opt_obj and opt_obj.option_code or req.get('event_id'),
             })
 
             balance_due = 0
@@ -468,6 +469,57 @@ class ReservationEvent(models.Model):
                 'PNR': booking_obj.pnr,
             })
 
+    def print_itinerary(self, data):
+        # jika panggil dari backend
+        if 'order_number' not in data:
+            data['order_number'] = self.name
+        if 'provider_type' not in data:
+            data['provider_type'] = self.provider_type_id.name
+
+        book_obj = self.env['tt.reservation.event'].search([('name', '=', data['order_number'])], limit=1)
+        datas = {'ids': book_obj.env.context.get('active_ids', [])}
+        res = book_obj.read()
+        res = res and res[0] or {}
+        datas['form'] = res
+        event_itinerary_id = book_obj.env.ref('tt_report_common.action_printout_itinerary_event')
+        if not book_obj.printout_itinerary_id:
+            if book_obj.agent_id:
+                co_agent_id = book_obj.agent_id.id
+            else:
+                co_agent_id = self.env.user.agent_id.id
+
+            if book_obj.user_id:
+                co_uid = book_obj.user_id.id
+            else:
+                co_uid = self.env.user.id
+
+            pdf_report = event_itinerary_id.report_action(book_obj, data=datas)
+            pdf_report['context'].update({
+                'active_model': book_obj._name,
+                'active_id': book_obj.id
+            })
+            pdf_report_bytes = event_itinerary_id.render_qweb_pdf(data=pdf_report)
+            res = book_obj.env['tt.upload.center.wizard'].upload_file_api(
+                {
+                    'filename': 'Event Itinerary %s.pdf' % book_obj.name,
+                    'file_reference': 'Event Itinerary',
+                    'file': base64.b64encode(pdf_report_bytes[0]),
+                    'delete_date': datetime.today() + timedelta(minutes=10)
+                },
+                {
+                    'co_agent_id': co_agent_id,
+                    'co_uid': co_uid
+                }
+            )
+            upc_id = book_obj.env['tt.upload.center'].search([('seq_id', '=', res['response']['seq_id'])], limit=1)
+            book_obj.printout_itinerary_id = upc_id.id
+        url = {
+            'type': 'ir.actions.act_url',
+            'name': "ZZZ",
+            'target': 'new',
+            'url': book_obj.printout_itinerary_id.url,
+        }
+        return url
 
 class TtReservationEventOption(models.Model):
     _name = 'tt.reservation.event.option'
