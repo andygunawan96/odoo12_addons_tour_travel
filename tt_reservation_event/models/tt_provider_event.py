7c066b589c5c77c4eaa698513fd9164e3fac8142
edo <bella@rodextravel.tours>
2020-06-09 16:32:18 +0700

    update provider view

--- tt_reservation_event/models/tt_provider_event.py
+++ tt_reservation_event/models/tt_provider_event.py
@@ -12,10 +12,12 @@ class TtProviderEvent(models.Model):
 
     pnr = fields.Char('PNR')
     pnr2 = fields.Char('PNR2')
+    reference = fields.Char('Reference', default='')
     provider_id = fields.Many2one('tt.provider', 'Provider')
     state = fields.Selection(variables.BOOKING_STATE, 'Status', default='draft')
     booking_id = fields.Many2one('tt.reservation.event', 'Order Number', ondelete='cascade')
     balance_due = fields.Float('Balance Due')
+    sequence = fields.Integer('Sequence')
     event_id = fields.Many2one('tt.master.event', 'Event')
     event_product_id = fields.Many2one('tt.event.option', 'Event Product')
     event_product = fields.Char('Product Type')
@@ -40,9 +42,9 @@ class TtProviderEvent(models.Model):
     hold_date = fields.Char('Hold Date')
     expired_date = fields.Datetime('Expired Date')
 
-    ticket_ids = fields.One2many('tt.ticket.event', 'provider_id', 'Ticket Number')
+    # ticket_ids = fields.One2many('tt.ticket.event', 'provider_id', 'Ticket Number')
 
-    error_msg = fields.Text('Message Error', readonly=True, states={'draft': [('readonly', False)]})
+    error_history_ids = fields.One2many('tt.reservation.err.history', 'res_id', 'Error history')
 
     notes = fields.Text('Notes', readonly=True, states={'draft': [('readonly', False)]})
 
