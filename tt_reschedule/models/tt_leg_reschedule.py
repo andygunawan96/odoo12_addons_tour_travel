c501f64915304bc75aa2a87201556f1037a509fa
samvi <it@rodextravel.tours>
2020-10-20 10:12:06 +0700

    *update reschedule leg* *update is adult birth date required*

--- tt_reschedule/models/tt_leg_reschedule.py
+++ tt_reschedule/models/tt_leg_reschedule.py
@@ -7,3 +7,9 @@ class TtLegReschedule(models.Model):
     _inherit = 'tt.leg.airline'
 
     segment_id = fields.Many2one('tt.segment.reschedule', 'Segment', ondelete='cascade')
+
+
+class TtSegmentRescheduleInherit(models.Model):
+    _inherit = 'tt.segment.reschedule'
+
+    leg_ids = fields.One2many('tt.leg.reschedule', 'segment_id', 'Legs')
