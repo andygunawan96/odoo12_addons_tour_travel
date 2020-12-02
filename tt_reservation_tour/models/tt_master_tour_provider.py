940d3ed018a2e99a23170b094a5a73bf06554d0b
Andy Gunawan <it@rodextravel.tours>
2020-12-02 10:18:50 +0700

    fix_reconcile_and_tour_lg_1

--- tt_reservation_tour/models/tt_master_tour_provider.py
+++ tt_reservation_tour/models/tt_master_tour_provider.py
@@ -1,4 +1,6 @@
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from datetime import datetime
 
 
 class MasterTourProvider(models.Model):
@@ -9,7 +11,52 @@ class MasterTourProvider(models.Model):
     provider_id = fields.Many2one('tt.provider', 'Provider', required=True)
     provider_type_id = fields.Many2one('tt.provider.type', 'Provider Type', related='provider_id.provider_type_id',
                                        store=True, readonly=True)
-    master_tour_id = fields.Many2one('tt.master.tour', 'Master Tour', required=True, readonly=True)
-    description = fields.Text('Provider Coverage Details')
-    letter_of_guarantee_ids = fields.One2many('tt.letter.guarantee', 'res_id', 'Letter of Guarantee(s)')
+    master_tour_id = fields.Many2one('tt.master.tour', 'Master Tour', readonly=True)
+    quantity = fields.Integer('Quantity', required=True, default=1)
+    currency_id = fields.Many2one('res.currency', 'Currency', default=lambda self: self.env.user.company_id.currency_id)
+    total_price = fields.Monetary('Total Price', default=0)
+    details = fields.Html('Details', default='')
+    is_lg_required = fields.Boolean('Is LG Required', readonly=True, compute='compute_is_lg_required')
+    letter_of_guarantee_ids = fields.One2many('tt.letter.guarantee', 'res_id', 'Letter of Guarantee(s)', readonly=True)
 
+    @api.onchange('provider_id')
+    def compute_is_lg_required(self):
+        for rec in self:
+            if rec.master_tour_id:
+                if rec.master_tour_id.tour_code:
+                    rec.is_lg_required = True if rec.provider_id.is_using_lg else False
+                else:
+                    rec.is_lg_required = False
+            else:
+                rec.is_lg_required = False
+
+    def generate_lg(self):
+        lg_exist = self.env['tt.letter.guarantee'].search([('res_model', '=', self._name), ('res_id', '=', self.id)])
+        if lg_exist:
+            raise UserError('Letter of Guarantee for this provider is already exist.')
+        else:
+            desc_str = self.master_tour_id.name + '<br/>'
+            pax_desc_str = ''
+            pax_amount = 0
+            price_per_mul = self.total_price / pax_amount / self.quantity
+            lg_vals = {
+                'res_model': self._name,
+                'res_id': self.id,
+                'provider_id': self.provider_id.id,
+                'type': 'lg',
+                'pax_description': pax_desc_str,
+                'multiplier': 'Pax',
+                'multiplier_amount': pax_amount,
+                'quantity': 'Qty',
+                'quantity_amount': self.quantity,
+                'currency_id': self.currency_id.id,
+                'price_per_mult': price_per_mul,
+                'price': self.total_price,
+            }
+            new_lg_obj = self.env['tt.letter.guarantee'].create(lg_vals)
+            line_vals = {
+                'lg_id': new_lg_obj.id,
+                'ref_number': self.master_tour_id.tour_code,
+                'description': desc_str + self.details
+            }
+            self.env['tt.letter.guarantee.lines'].create(line_vals)
