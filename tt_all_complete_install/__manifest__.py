e6d3b6983cfbfe8fc3d62a4fafd7c14b18dbf504
Joshua <it@rodextravel.tours>
2020-12-23 10:34:46 +0700

    revisi all complete install

--- tt_all_complete_install/__manifest__.py
+++ tt_all_complete_install/__manifest__.py
@@ -19,26 +19,42 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'base_setup',
+    'depends': ['base', 'base_setup','web_m2x_options',
                 'tt_api_management',
                 'tt_reschedule',
                 'tt_merge_record',
-                'tt_report_selling',
+
                 'tt_public_holiday',
-                'tt_frontend_banner', 'tt_bank_transaction',
-                'tt_billing_statement', 'tt_agent_report_billing',
+                'tt_frontend_banner',
+                'tt_bank_transaction',
+                'tt_billing_statement',
+
                 'tt_reservation_report_airline',
-                'tt_agent_report_invoice', 'tt_agent_report_offline',
-                'tt_agent_report_recap_reservation', 'tt_agent_report_recap_transaction',
+
                 'tt_agent_sales_activity',
-                # 'tt_agent_sales_agent_registration',
-                'tt_agent_sales_airline', 'tt_agent_sales_hotel', 'tt_agent_sales_offline',
-                #'tt_agent_sales_passport',
+                'tt_agent_sales_airline',
+                'tt_agent_sales_event',
+                'tt_agent_sales_hotel',
+                'tt_agent_sales_offline',
+                'tt_agent_sales_ppob',
                 'tt_agent_sales_tour',
-                'tt_agent_sales_train', 'tt_agent_sales_visa', 'web_m2x_options',
+                'tt_agent_sales_train',
+                'tt_agent_sales_visa',
+
                 'tt_agent_report_balance',
-                #'tt_agent_report_passport',
-                'tt_agent_report_visa'
+                'tt_agent_report_billing',
+                'tt_agent_report_invoice',
+                'tt_agent_report_offline',
+                'tt_agent_report_recap_reservation',
+                'tt_agent_report_recap_transaction',
+                'tt_agent_report_visa',
+
+                'tt_customer_report_birthday',
+                'tt_customer_report_performance',
+
+                'tt_report_dashboard',
+                'tt_report_selling',
+                'tt_report_vendor_event',
     ],
 
     # always loaded
