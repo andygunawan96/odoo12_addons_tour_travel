bd705786fbaddacde80f06bfac1409e051b5a5d2
Joshua <it@rodextravel.tours>
2021-06-16 16:30:22 +0700

    get price phc

--- tt_reservation_phc/models/tt_reservation_phc.py
+++ tt_reservation_phc/models/tt_reservation_phc.py
@@ -174,6 +174,14 @@ class Reservationphc(models.Model):
     def create_refund(self):
         self.state = 'refund'
 
+    # COMMISSION_PER_PAX_ANTIGEN = 25000  ## komisi agent /pax
+    # COMMISSION_PER_PAX_PCR_HC = 120000  ## komisi agent /pax
+    # COMMISSION_PER_PAX_PCR_DT = 80000  ## komisi agent /pax
+    # BASE_PRICE_PER_PAX_ANTIGEN = 150000  ## harga 1 /pax
+    # BASE_PRICE_PER_PAX_PCR_HC = 750000  ## harga 1 /pax
+    # BASE_PRICE_PER_PAX_PCR_DT = 750000  ## harga 1 /pax
+    # SINGLE_SUPPLEMENT = 25000  ## 1 orang
+    # OVERTIME_SURCHARGE = 50000  ## lebih dari 18.00 /pax
     def get_price_phc_api(self, req, context):
         #parameter
         #timeslot_list
@@ -186,24 +194,30 @@ class Reservationphc(models.Model):
                 overtime_surcharge = True
                 break
 
-        carrier_obj = self.env['tt.transport.carrier'].search([('code','=',req['carrier_code'])])
+        carrier_id = self.env['tt.transport.carrier'].search([('code','=',req['carrier_code'])]).id
         single_suplement = False
         if req['pax_count'] <= 1 and \
-                carrier_obj.id not in [self.env.ref('tt_reservation_phc.tt_transport_carrier_phc_drive_thru_antigen').id,
+                carrier_id not in [self.env.ref('tt_reservation_phc.tt_transport_carrier_phc_drive_thru_antigen').id,
                                     self.env.ref('tt_reservation_phc.tt_transport_carrier_phc_drive_thru_pcr').id]:
             single_suplement = True
 
-        if carrier_obj.id in [self.env.ref('tt_reservation_phc.tt_transport_carrier_phc_drive_thru_antigen').id,
+        if carrier_id in [self.env.ref('tt_reservation_phc.tt_transport_carrier_phc_drive_thru_antigen').id,
                               self.env.ref('tt_reservation_phc.tt_transport_carrier_phc_home_care_antigen').id]:
             base_price = BASE_PRICE_PER_PAX_ANTIGEN
+            commission_price = COMMISSION_PER_PAX_ANTIGEN
+        elif carrier_id in [self.env.ref('tt_reservation_phc.tt_transport_carrier_phc_drive_thru_pcr')]:
+            base_price = BASE_PRICE_PER_PAX_PCR_DT
+            commission_price = COMMISSION_PER_PAX_PCR_DT
         else:
-            base_price = BASE_PRICE_PER_PAX_PCR
+            base_price = BASE_PRICE_PER_PAX_PCR_HC
+            commission_price = COMMISSION_PER_PAX_PCR_HC
 
-        total_price = base_price + (overtime_surcharge and OVERTIME_SURCHARGE or 0) + (single_suplement and SINGLE_SUPPLEMENT or 0)
+        extra_charge_per_pax = (overtime_surcharge and OVERTIME_SURCHARGE or 0) + (single_suplement and SINGLE_SUPPLEMENT or 0)
         return ERR.get_no_error({
             "pax_count": req['pax_count'],
-            "base_price_per_pax": total_price,
-            "commission_per_pax": COMMISSION_PER_PAX
+            "base_price_per_pax": base_price,
+            "extra_price_per_pax": extra_charge_per_pax,
+            "commission_per_pax": commission_price
         })
 
     def create_booking_phc_api(self, req, context):
