939282fe54443d905fc38d805e7b41883359bb41
Joshua <it@rodextravel.tours>
2021-07-05 09:37:26 +0700

    limiter slot adult

--- tt_reservation_phc/models/tt_timeslot_phc.py
+++ tt_reservation_phc/models/tt_timeslot_phc.py
@@ -82,6 +82,7 @@ class TtTimeslotphc(models.Model):
     def _compute_used_counter(self):
         for rec in self:
             used_count = 0
+            adult_count = 0
             for rec2 in rec.booking_used_ids.filtered(lambda x: x.state in ['booked', 'issued']):
                 used_count += 1
                 adult_count = rec2.adult
