12ee53e65dbf45cc490e4209c5c250316ce59d73
Joshua <it@rodextravel.tours>
2021-07-08 14:58:05 +0700

    adult count di hitung supaya error

--- tt_reservation_phc/models/tt_timeslot_phc.py
+++ tt_reservation_phc/models/tt_timeslot_phc.py
@@ -167,9 +167,9 @@ class TtTimeslotphc(models.Model):
             })
         return ERR.get_no_error(timeslot_dict)
 
-    def get_availability(self):
+    def get_availability(self,adult_count=1):
         ## availability adult & pcr
-        availability = self.used_adult_count < self.total_adult_timeslot and self.used_pcr_count < self.total_pcr_timeslot
+        availability = self.used_adult_count + adult_count < self.total_adult_timeslot and self.used_pcr_count + adult_count < self.total_pcr_timeslot
 
         if self.timeslot_type == 'drive_thru':
             return availability
