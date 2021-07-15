66d68baf5d2a513eb38955db00016b1429e5610c
ivan <ivan@rodextravel.tours>
2021-07-15 10:01:29 +0700

    update_nama_rekening_n_duplicate_identity_number_phc

--- tt_reservation_phc/models/tt_reservation_phc.py
+++ tt_reservation_phc/models/tt_reservation_phc.py
@@ -265,9 +265,9 @@ class Reservationphc(models.Model):
 
         try:
             ##validator pax kembar dan belum verified di PHC
-            # duplicate_pax_list = self.env['tt.reservation.passenger.phc'].find_duplicate_passenger_new_order(passengers)
-            # if duplicate_pax_list:
-            #     raise RequestException(1026,additional_message="Duplicate Identity Number with other bookings")
+            duplicate_pax_list = self.env['tt.reservation.passenger.phc'].find_duplicate_passenger_new_order(passengers)
+            if duplicate_pax_list:
+                raise RequestException(1026,additional_message="Duplicate Identity Number with other bookings")
 
             values = self._prepare_booking_api(booking_data,context)
             booker_obj = self.create_booker_api(booker,context)
