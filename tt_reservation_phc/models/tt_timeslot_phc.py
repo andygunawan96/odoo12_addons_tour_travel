695e16978ad72cb3158dcf9c190ffe512b3f7516
Joshua <it@rodextravel.tours>
2021-07-15 10:36:43 +0700

    testing concurrent

--- tt_reservation_phc/models/tt_timeslot_phc.py
+++ tt_reservation_phc/models/tt_timeslot_phc.py
@@ -62,26 +62,6 @@ class TtTimeslotphc(models.Model):
 
     agent_id = fields.Many2one('tt.agent', 'Agent')
 
-    def write(self, vals):
-        try:
-            super(TtTimeslotphc, self).write(vals)
-        except Exception as e:
-            _logger.error("###MASUK concurrent timeslot phc write")
-            if "concurrent update" in str(e):
-                raise RequestException(1036)
-            else:
-                raise e
-
-    def update(self, values):
-        try:
-            super(TtTimeslotphc, self).update(values)
-        except Exception as e:
-            _logger.error("###MASUK concurrent timeslot phc update")
-            if "concurrent update" in str(e):
-                raise RequestException(1036)
-            else:
-                raise e
-
     @api.depends('datetimeslot')
     def _compute_timeslot_display_name(self):
         for rec in self:
@@ -103,27 +83,21 @@ class TtTimeslotphc(models.Model):
     @api.depends('booking_used_ids','booking_used_ids.state')
     def _compute_used_counter(self):
         for rec in self:
-            try:
-                used_count = 0
-                adult_count = 0
-                pcr_count = 0
-                for rec2 in rec.booking_used_ids.filtered(lambda x: x.state in ['booked', 'issued']):
-                    used_count += 1
-                    adult_count += rec2.adult
-                    if 'PCR' in rec2.carrier_name:
-                        pcr_count += rec2.adult
-                if used_count != rec.used_count:
-                    rec.used_count = used_count
-                if rec.used_adult_count != adult_count:
-                    rec.used_adult_count = adult_count
-                if rec.used_pcr_count != pcr_count:
-                    rec.used_pcr_count = pcr_count
-            except Exception as e:
-                _logger.error("###MASUK concurrent timeslot phc compute")
-                if "concurrent update" in str(e):
-                    raise RequestException(1036)
-                else:
-                    raise e
+            used_count = 0
+            adult_count = 0
+            pcr_count = 0
+            for rec2 in rec.booking_used_ids.filtered(lambda x: x.state in ['booked', 'issued']):
+                used_count += 1
+                adult_count += rec2.adult
+                if 'PCR' in rec2.carrier_name:
+                    pcr_count += rec2.adult
+            if used_count != rec.used_count:
+                rec.used_count = used_count
+            if rec.used_adult_count != adult_count:
+                rec.used_adult_count = adult_count
+            if rec.used_pcr_count != pcr_count:
+                rec.used_pcr_count = pcr_count
+
 
 
     def mass_close_timeslot(self):
