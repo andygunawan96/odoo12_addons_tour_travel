b7217282f2a42983c4d9fa6ed9680589a4821dcd
Andy Gunawan <it@rodextravel.tours>
2021-08-24 15:13:40 +0700

    remove_print

--- tt_accounting/wizard/tt_reimburse_commission_wizard.py
+++ tt_accounting/wizard/tt_reimburse_commission_wizard.py
@@ -182,11 +182,9 @@ class ReimburseCommissionWizard(models.TransientModel):
                         for counter, tier in enumerate(tier_list):
                             if counter == len(tier_list) - 1:
                                 rac_amt = (tier.rac_amount / tier.denominator) * tier_nta_amount['ADT']
-                                print('Selected Tier: ' + str(tier.lower_limit))
                             elif total_tier_price < tier.lower_limit and total_tier_price + tier_nta_amount['ADT'] >= tier.lower_limit:
                                 iter_list = [tier]
                                 for co in range(counter+1, len(tier_list)):
-                                    print(tier_list[co].lower_limit)
                                     if total_tier_price < tier_list[co].lower_limit:
                                         iter_list.append(tier_list[co])
                                     else:
@@ -195,22 +193,17 @@ class ReimburseCommissionWizard(models.TransientModel):
                                 rac_amt = 0
                                 temp_total = total_tier_price
                                 temp_nta = tier_nta_amount['ADT']
-                                print(iter_list[::-1])
                                 for idx, iter_item in enumerate(iter_list[::-1]):
                                     if idx < len(iter_list[::-1]) - 1:
                                         sub_amt = iter_list[::-1][idx+1].lower_limit - temp_total
                                         temp_total += sub_amt
                                         temp_nta -= sub_amt
                                         rac_amt += (iter_item.rac_amount / iter_item.denominator) * sub_amt
-                                        print(sub_amt)
-                                        print('Iteration Selected Tier: ' + str(iter_item.lower_limit))
                                     else:
                                         rac_amt += (tier.rac_amount / tier.denominator) * temp_nta
-                                        print('Final Selected Tier: ' + str(tier.lower_limit))
                                 break
                             elif total_tier_price + tier_nta_amount['ADT'] >= tier.lower_limit:
                                 rac_amt = (tier.rac_amount / tier.denominator) * tier_nta_amount['ADT']
-                                print('Selected Tier: ' + str(tier.lower_limit))
                                 break
                         subs_rac = (self.rac_amount / self.denominator) * nta_amount['ADT']
                         rac_amt -= subs_rac
