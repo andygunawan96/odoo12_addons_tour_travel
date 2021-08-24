6ff687bb863fe525fc2864e13026432d0c107887
Andy Gunawan <it@rodextravel.tours>
2021-08-24 16:33:42 +0700

    reservation_bus_ipank

---
+++ tt_reservation_bus/models/tt_provider_bus.py
@@ -0,0 +1,388 @@
+from odoo import api, fields, models
+from odoo.exceptions import UserError
+from ...tools import variables
+from datetime import datetime
+import json
+
+
+class TtProviderBus(models.Model):
+    _name = 'tt.provider.bus'
+    _rec_name = 'pnr'
+    _order = 'departure_date'
+    _description = 'Provider Bus'
+
+    pnr = fields.Char('PNR')
+    pnr2 = fields.Char('PNR2')
+    provider_id = fields.Many2one('tt.provider','Provider')
+    state = fields.Selection(variables.BOOKING_STATE, 'Status', default='draft')
+    booking_id = fields.Many2one('tt.reservation.bus', 'Order Number', ondelete='cascade')
+    sequence = fields.Integer('Sequence')
+    balance_due = fields.Float('Balance Due')
+    origin_id = fields.Many2one('tt.destinations', 'Origin')
+    destination_id = fields.Many2one('tt.destinations', 'Destination')
+    departure_date = fields.Char('Departure Date')
+    return_date = fields.Char('Return Date')
+    arrival_date = fields.Char('Arrival Date')
+
+    sid_issued = fields.Char('SID Issued')#signature generate sendiri
+
+    cost_service_charge_ids = fields.One2many('tt.service.charge', 'provider_bus_booking_id', 'Cost Service Charges')
+
+    currency_id = fields.Many2one('res.currency', 'Currency', readonly=True, states={'draft': [('readonly', False)]},
+                                  default=lambda self: self.env.user.company_id.currency_id)
+
+    # total = fields.Monetary(string='Total', readonly=True)
+    # total_fare = fields.Monetary(string='Total Fare', compute="_compute_provider_total_fare", readonly=True)
+    # total_orig = fields.Monetary(string='Total (Original)', readonly=True)
+    promotion_code = fields.Char(string='Promotion Code')
+
+
+    # Booking Progress
+    booked_uid = fields.Many2one('res.users', 'Booked By')
+    booked_date = fields.Datetime('Booking Date')
+    issued_uid = fields.Many2one('res.users', 'Issued By')
+    issued_date = fields.Datetime('Issued Date')
+    hold_date = fields.Char('Hold Date')
+    expired_date = fields.Datetime('Expired Date')
+    cancel_uid = fields.Many2one('res.users', 'Cancel By')
+    cancel_date = fields.Datetime('Cancel Date')
+    #
+    refund_uid = fields.Many2one('res.users', 'Refund By')
+    refund_date = fields.Datetime('Refund Date')
+
+    ticket_ids = fields.One2many('tt.ticket.bus', 'provider_id', 'Ticket Number')
+
+    # is_ledger_created = fields.Boolean('Ledger Created', default=False, readonly=True, states={'draft': [('readonly', False)]})
+
+    error_history_ids = fields.One2many('tt.reservation.err.history','res_id','Error History', domain=[('res_model','=','tt.provider.bus')])
+
+    total_price = fields.Float('Total Price', default=0)
+
+    #reconcile purpose#
+    reconcile_line_id = fields.Many2one('tt.reconcile.transaction.lines','Reconciled')
+    reconcile_time = fields.Datetime('Reconcile Time')
+    ##
+
+    ##button function
+    def action_set_to_issued_from_button(self, payment_data={}):
+        if self.state == 'issued':
+            raise UserError("Has been Issued.")
+        self.write({
+            'state': 'issued',
+            'issued_uid': self.env.user.id,
+            'issued_date': datetime.now()
+        })
+        self.booking_id.check_provider_state({'co_uid': self.env.user.id}, [], False, payment_data)
+
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
+
+    def action_force_issued_from_button(self, payment_data={}):
+        if self.state == 'issued':
+            raise UserError("Has been Issued.")
+
+        req = {
+            'book_id': self.booking_id.id,
+            'member': payment_data.get('member'),
+            'acquirer_seq_id': payment_data.get('acquirer_seq_id'),
+        }
+        context = {
+            'co_agent_id': self.booking_id.agent_id.id,
+            'co_agent_type_id': self.booking_id.agent_type_id.id,
+            'co_uid': self.env.user.id
+        }
+        payment_res = self.booking_id.payment_reservation_api('bus', req, context)
+        if payment_res['error_code'] != 0:
+            raise UserError(payment_res['error_msg'])
+
+        # balance_res = self.env['tt.agent'].check_balance_limit_api(self.booking_id.agent_id.id,self.booking_id.agent_nta)
+        # if balance_res['error_code'] != 0:
+        #     raise UserError("Balance not enough.")
+        #
+        # self.action_create_ledger(self.env.user.id)
+        self.action_set_to_issued_from_button(payment_data)
+
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
+
+    def action_set_to_book_from_button(self):
+        if self.state == 'booked':
+            raise UserError("Has been Booked.")
+
+        self.write({
+            'state': 'booked',
+            'booked_uid': self.env.user.id,
+            'booked_date': datetime.now()
+        })
+
+        self.booking_id.check_provider_state({'co_uid':self.env.user.id})
+
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
+
+    def action_reverse_ledger_from_button(self):
+        if self.state == 'fail_refunded':
+            raise UserError("Cannot refund, this PNR has been refunded.")
+
+        ##fixme salahhh, ini ke reverse semua provider bukan provider ini saja
+        ## ^ harusnay sudah fix
+        for rec in self.booking_id.ledger_ids:
+            if rec.pnr == self.pnr and not rec.is_reversed:
+                rec.reverse_ledger()
+
+        for rec in self.cost_service_charge_ids:
+            rec.is_ledger_created = False
+
+        self.write({
+            'state': 'fail_refunded',
+            # 'is_ledger_created': False,
+            'refund_uid': self.env.user.id,
+            'refund_date': datetime.now()
+        })
+
+        self.booking_id.check_provider_state({'co_uid':self.env.user.id})
+
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
+
+    ###
+    def action_booked_api_bus(self, provider_data, api_context):
+        for rec in self:
+            rec.write({
+                'pnr': provider_data['pnr'],
+                'pnr2': provider_data['pnr2'],
+                'state': 'booked',
+                'booked_uid': api_context['co_uid'],
+                'booked_date': fields.Datetime.now(),
+                'hold_date': datetime.strptime(provider_data['hold_date'],"%Y-%m-%d %H:%M:%S"),
+                'balance_due': provider_data['balance_due'],
+                'total_price': provider_data['total_price']
+            })
+
+    def action_issued_api_bus(self,context):
+        for rec in self:
+            rec.write({
+                'state': 'issued',
+                'issued_date': datetime.now(),
+                'issued_uid': context['co_uid'],
+                'sid_issued': context['signature'],
+                'balance_due': 0
+            })
+
+    def action_failed_booked_api_bus(self,err_code,err_msg):
+        for rec in self:
+            rec.write({
+                'state': 'fail_booked',
+                'error_history_ids': [(0,0,{
+                    'res_model': self._name,
+                    'res_id': self.id,
+                    'error_code': err_code,
+                    'error_msg': err_msg
+                })]
+            })
+
+    def action_failed_issued_api_bus(self,err_code,err_msg):
+        for rec in self:
+            rec.write({
+                'state': 'fail_issued',
+                'error_history_ids': [(0,0,{
+                    'res_model': self._name,
+                    'res_id': self.id,
+                    'error_code': err_code,
+                    'error_msg': err_msg
+                })]
+            })
+
+    def action_expired(self):
+        self.state = 'cancel2'
+
+    def action_refund(self, check_provider_state=False):
+        self.state = 'refund'
+        if check_provider_state:
+            self.booking_id.check_provider_state({'co_uid': self.env.user.id})
+
+    def action_cancel(self):
+        self.cancel_date = fields.Datetime.now()
+        self.cancel_uid = self.env.user.id
+        self.state = 'cancel'
+
+    def create_ticket_api(self,passengers,pnr=""):
+        ticket_list = []
+
+        #################
+        for passenger in self.booking_id.passenger_ids:
+            passenger.is_ticketed = False
+        #################
+
+        for psg in passengers:
+            psg_obj = self.booking_id.passenger_ids.filtered(lambda x: x.sequence == psg.get('sequence'))
+            if not psg_obj.is_ticketed:
+                psg_obj.is_ticketed = True
+                ticket_list.append((0, 0, {
+                    'pax_type': psg.get('pax_type'),
+                    'ticket_number': psg.get('ticket_number'),
+                    'passenger_id': psg_obj.id
+                }))
+
+        self.write({
+            'ticket_ids': ticket_list
+        })
+
+    def update_ticket_api(self,passengers):##isi ticket number
+        ticket_not_found = []
+        for psg in passengers:
+            ticket_found = False
+            for ticket in self.ticket_ids:
+                psg_name = ticket.passenger_id.name.replace(' ','').lower()
+                if ('%s%s' % (psg['first_name'], psg['last_name'])).replace(' ','').lower() in [psg_name, psg_name*2] and not ticket.ticket_number:
+                    ticket.write({
+                        'ticket_number': psg.get('ticket_number','')
+                    })
+                    ticket_found = True
+                    ticket.passenger_id.is_ticketed = True
+                    break
+            if not ticket_found:
+                ticket_not_found.append(psg)
+
+        for psg in ticket_not_found:
+            self.write({
+                'ticket_ids': [(0,0,{
+                    'ticket_number': psg.get('ticket_number'),
+                    'pax_type': psg.get('pax_type'),
+                })]
+            })
+
+    def create_service_charge(self, service_charge_vals):
+        service_chg_obj = self.env['tt.service.charge']
+        currency_obj = self.env['res.currency']
+
+        for scs in service_charge_vals:
+            # update 7 Feb 2020 maximum per pax sesuai dengan pax_count dari service charge
+            # scs['pax_count'] = 0
+            scs_pax_count = 0
+            scs['passenger_bus_ids'] = []
+            scs['total'] = 0
+            scs['currency_id'] = currency_obj.get_id(scs.get('currency'))
+            scs['foreign_currency_id'] = currency_obj.get_id(scs.get('foreign_currency'))
+            scs['provider_bus_booking_id'] = self.id
+            for psg in self.ticket_ids:
+                if scs['pax_type'] == psg.pax_type and scs_pax_count < scs['pax_count']:
+                    scs['passenger_bus_ids'].append(psg.passenger_id.id)
+                    # scs['pax_count'] += 1
+                    scs_pax_count += 1
+                    scs['total'] += scs['amount']
+            scs.pop('currency')
+            scs.pop('foreign_currency')
+            scs['passenger_bus_ids'] = [(6,0,scs['passenger_bus_ids'])]
+            scs['description'] = self.pnr
+            service_chg_obj.create(scs)
+
+        # "sequence": 1,
+        # "charge_code": "fare",
+        # "charge_type": "FARE",
+        # "currency": "IDR",
+        # "amount": 4800000,
+        # "foreign_currency": "IDR",
+        # "foreign_amount": 4800000,
+        # "pax_count": 3,
+        # "pax_type": "ADT",
+        # "total": 14400000
+
+    def delete_service_charge(self):
+        ledger_created = False
+        for rec in self.cost_service_charge_ids.filtered(lambda x: x.is_extra_fees == False):
+            if rec.is_ledger_created:
+                ledger_created = True
+            else:
+                rec.unlink()
+        return ledger_created
+    # @api.depends('cost_service_charge_ids')
+    # def _compute_total(self):
+    #     for rec in self:
+    #         total = 0
+    #         total_orig = 0
+    #         for sc in rec.cost_service_charge_ids:
+    #             if sc.charge_code.find('r.ac') < 0:
+    #                 total += sc.total
+    #             # total_orig adalah NTA
+    #             total_orig += sc.total
+    #         rec.write({
+    #             'total': total,
+    #             'total_orig': total_orig
+    #         })
+
+    def action_create_ledger(self,issued_uid,pay_method=None):
+        return self.env['tt.ledger'].action_create_ledger(self,issued_uid)
+        # else:
+        #     raise UserError("Cannot create ledger, ledger has been created before.")
+
+    def to_dict(self):
+        journey_list = []
+        for rec in self.journey_ids:
+            journey_list.append(rec.to_dict())
+        ticket_list = []
+        for rec in self.ticket_ids:
+            ticket_list.append(rec.to_dict())
+        res = {
+            'pnr': self.pnr and self.pnr or '',
+            'pnr2': self.pnr2 and self.pnr2 or '',
+            'provider': self.provider_id.code,
+            'provider_id': self.id,
+            'state': self.state,
+            'state_description': variables.BOOKING_STATE_STR[self.state],
+            'sequence': self.sequence,
+            'balance_due': self.balance_due,
+            'total_price': self.total_price,
+            'origin': self.origin_id.code,
+            'destination': self.destination_id.code,
+            'departure_date': self.departure_date,
+            'arrival_date': self.arrival_date,
+            'journeys': journey_list,
+            'currency': self.currency_id.name,
+            'hold_date': self.hold_date and self.hold_date or '',
+            'tickets': ticket_list,
+            'error_msg': self.error_history_ids and self.error_history_ids[-1].error_msg or ''
+        }
+
+        return res
+
+    def get_carrier_name(self):
+        carrier_names = set([])
+        for journey in self.journey_ids:
+            if journey.carrier_id:
+                carrier_names.add(journey.carrier_id.name)
+        return carrier_names
+
+    # def get_cost_service_charges(self):
+    #     sc_value = {}
+    #     for p_sc in self.cost_service_charge_ids:
+    #         p_charge_type = p_sc.charge_type
+    #         p_pax_type = p_sc.pax_type
+    #         if p_charge_type == 'RAC' and p_sc.charge_code !=  'rac':
+    #             continue
+    #         if not sc_value.get(p_pax_type):
+    #             sc_value[p_pax_type] = {}
+    #         if not sc_value[p_pax_type].get(p_charge_type):
+    #             sc_value[p_pax_type][p_charge_type] = {}
+    #             sc_value[p_pax_type][p_charge_type].update({
+    #                 'amount': 0,
+    #                 'foreign_amount': 0
+    #             })
+    #         sc_value[p_pax_type][p_charge_type].update({
+    #             'charge_code': p_sc.charge_code,
+    #             'currency': p_sc.currency_id.name,
+    #             'pax_count': p_sc.pax_count,
+    #             'total': p_sc.total,
+    #             'foreign_currency': p_sc.foreign_currency_id.name,
+    #             'amount': sc_value[p_pax_type][p_charge_type]['amount'] + p_sc.amount,
+    #             'foreign_amount': sc_value[p_pax_type][p_charge_type]['foreign_amount'] + p_sc.foreign_amount,
+    #         })
+    #     return sc_value
