0f3701f12b50060e5dfa6d7bc07ce671caa6ccbe
Andy Gunawan <it@rodextravel.tours>
2021-08-27 15:58:25 +0700

    fix_typos

--- tt_in_api_connector/models/tt_bus_api.py
+++ tt_in_api_connector/models/tt_bus_api.py
@@ -1,7 +1,7 @@
 from odoo import api,models,fields
 from ...tools.ERR import RequestException
 
-class TtTrainApiCon(models.Model):
+class TtBusApiCon(models.Model):
     _name = 'tt.bus.api.con'
     _inherit = 'tt.api.con'
 
@@ -10,20 +10,10 @@ class TtTrainApiCon(models.Model):
     def action_call(self,table_obj,action,data,context):
         if action == 'get_config':
             res = self.env['tt.master.bus.station'].get_config_api()
-        elif action == 'get_availability':
-            res = self.env['tt.timeslot.phc'].get_available_timeslot_api(data, context)
-        elif action == 'get_price':
-            res = table_obj.get_price_phc_api(data,context)
         elif action == 'create_booking':
             res = self.env['tt.reservation.bus'].create_booking_bus_api(data,context)
         elif action == 'edit_passenger_verify_api':
             res = table_obj.edit_passenger_verify_api(data,context)
-        elif action == 'update_pnr_provider':
-            res = table_obj.update_pnr_provider_phc_api(data,context)
-        elif action == 'get_booking':
-            res = table_obj.get_booking_phc_api(data,context)
-        elif action == 'payment':
-            res = table_obj.payment_phc_api(data,context)
         elif action == 'get_transaction_by_analyst':
             res = table_obj.get_transaction_by_analyst_api(data,context)
         elif action == 'update_data_verif':
