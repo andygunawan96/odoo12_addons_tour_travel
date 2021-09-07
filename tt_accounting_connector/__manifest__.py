aeae5193fa5c0028d4e5cb56581cede7cbe9e292
Andy Gunawan <it@rodextravel.tours>
2021-09-07 11:14:34 +0700

    update_jasaweb_accounting

--- tt_accounting_connector/__manifest__.py
+++ tt_accounting_connector/__manifest__.py
@@ -17,6 +17,7 @@ Key Features
     'depends' : ['tt_base', 'tt_accounting'],
     'data': [
         'data/ir_cron_data.xml',
+        'views/tt_payment_acquirer_views.xml',
         'views/accounting_queue_views.xml',
         'security/ir.model.access.csv'
     ],
