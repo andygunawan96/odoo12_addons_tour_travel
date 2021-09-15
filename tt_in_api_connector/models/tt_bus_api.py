cc2a6dc2da4f7102f6cd10d84c2ecdf7877029b4
ivan <ivan@rodextravel.tours>
2021-09-15 15:15:51 +0700

    update_payment

--- tt_in_api_connector/models/tt_bus_api.py
+++ tt_in_api_connector/models/tt_bus_api.py
@@ -15,7 +15,9 @@ class TtBusApiCon(models.Model):
         elif action == 'get_booking':
             res = self.env['tt.reservation.bus'].get_booking_bus_api(data,context)
         elif action == 'update_pnr_provider':
-            res = self.env['tt.reservation.bus'].update_pnr_provider_bus_api(data,context)
+            res = self.env['tt.reservation.bus'].update_pnr_provider_bus_api(data, context)
+        elif action == 'payment':
+            res = table_obj.payment_bus_api(data,context)
         else:
             raise RequestException(999)
         return res
