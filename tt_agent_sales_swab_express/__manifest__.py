a8cd6505a896c0183337b5954ef24a1328c16917
ivan <ivan@rodextravel.tours>
2021-10-08 16:34:51 +0700

    agent_sales_swab_express_n_lab_pintar

--- tt_agent_sales_swab_express/__manifest__.py
+++ tt_agent_sales_swab_express/__manifest__.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Tour & Travel - Agent Sales Medical',
+    'name': 'Tour & Travel - Agent Sales Swab Express',
     'version': '2.0',
     'category': 'Billing',
-    'summary': 'Agent Sales Medical Module',
+    'summary': 'Agent Sales Swab Express Module',
     'sequence': 30,
     'description': """
-Tour & Travel - Agent Sales Medical
+Tour & Travel - Agent Sales Swab Express
 =================================
 Key Features
 ------------
     """,
     'author': "PT Roda Express Sukses Mandiri",
     'website': "rodextravel.tours",
-    'depends': ['base', 'base_setup', 'tt_agent_sales','tt_reservation_medical'],
+    'depends': ['base', 'base_setup', 'tt_agent_sales','tt_reservation_swab_express'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/tt_reservation_medical.xml'
+        'views/tt_reservation_swab_express.xml'
 
     ],
     'demo': [],
