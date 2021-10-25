7eddc8e627fbc9e2e29c3c614504164c6d3fae77
Andy Gunawan <it@rodextravel.tours>
2021-10-25 15:28:36 +0700

    bus_ticket_and_itinerary

--- tt_reservation_bus/models/tt_reservation_bus.py
+++ tt_reservation_bus/models/tt_reservation_bus.py
@@ -924,8 +924,8 @@ class TtReservationBus(models.Model):
             pdf_report_bytes = bus_itinerary_id.render_qweb_pdf(data=pdf_report)
             res = book_obj.env['tt.upload.center.wizard'].upload_file_api(
                 {
-                    'filename': 'bus Itinerary %s.pdf' % book_obj.name,
-                    'file_reference': 'bus Itinerary',
+                    'filename': 'Bus Itinerary %s.pdf' % book_obj.name,
+                    'file_reference': 'Bus Itinerary',
                     'file': base64.b64encode(pdf_report_bytes[0]),
                     'delete_date': datetime.today() + timedelta(minutes=10)
                 },
