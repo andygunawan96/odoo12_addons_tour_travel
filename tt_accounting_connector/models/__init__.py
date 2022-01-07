c78516ef74c5771c01ea3634c4f39356fcaedead
Andy Gunawan <it@rodextravel.tours>
2022-01-07 11:13:20 +0700

    accounting_connector_add_agent_is_sync

--- tt_accounting_connector/models/__init__.py
+++ tt_accounting_connector/models/__init__.py
@@ -1,6 +1,7 @@
 from . import tt_payment_acquirer
 from . import tt_accounting_connector
 from . import tt_accounting_queue
+from . import tt_agent
 from . import tt_ledger
 from . import tt_top_up
 from . import tt_refund
--- tt_accounting_connector_tour/models/tt_reservation_tour.py
+++ tt_accounting_connector_tour/models/tt_reservation_tour.py
@@ -16,46 +16,47 @@ class TtReservationTour(models.Model):
             base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
             pay_acq = self.env['payment.acquirer'].search([('seq_id', '=', self.payment_method)], limit=1)
             ledger_list = []
-            for rec in self.ledger_ids:
-                if not rec.is_sent_to_acc:
-                    if rec.transaction_type == 2:
-                        trans_type = 'Transport Booking'
-                    elif rec.transaction_type == 3:
-                        trans_type = 'Commission'
-                        if rec.agent_type_id.id == self.env.ref('tt_base.agent_type_ho').id:
-                            trans_type += ' HO'
+            if self.agent_id.is_sync_to_acc:
+                for rec in self.ledger_ids:
+                    if not rec.is_sent_to_acc:
+                        if rec.transaction_type == 2:
+                            trans_type = 'Transport Booking'
+                        elif rec.transaction_type == 3:
+                            trans_type = 'Commission'
+                            if rec.agent_type_id.id == self.env.ref('tt_base.agent_type_ho').id:
+                                trans_type += ' HO'
+                            else:
+                                trans_type += ' Channel'
                         else:
-                            trans_type += ' Channel'
-                    else:
-                        trans_type = ''
+                            trans_type = ''
 
-                    ledger_list.append({
-                        'reference_number': rec.ref and rec.ref or '',
-                        'name': rec.name and rec.name or '',
-                        'debit': rec.debit and rec.debit or 0,
-                        'credit': rec.credit and rec.credit or 0,
-                        'currency_id': rec.currency_id and rec.currency_id.name or '',
-                        'create_date': rec.create_date and datetime.strftime(rec.create_date, '%Y-%m-%d %H:%M:%S') or '',
-                        'date': rec.date and datetime.strftime(rec.date, '%Y-%m-%d') or '',
-                        'create_uid': rec.create_uid and rec.create_uid.name or '',
-                        'commission': 0.0,
-                        'description': rec.description and rec.description or '',
-                        'agent_id': rec.agent_id and rec.agent_id.name,
-                        'company_sender': rec.agent_id and rec.agent_id.name,
-                        'company_receiver': self.env.ref('tt_base.rodex_ho').name,
-                        'state': 'Done',
-                        'display_provider_name': rec.display_provider_name and rec.display_provider_name or '',
-                        'pnr': rec.pnr and rec.pnr or '',
-                        'url_legacy': base_url + '/web#id=' + str(rec.id) + '&model=tt.ledger&view_type=form',
-                        'transaction_type': trans_type,
-                        'transport_type': self.provider_type_id and self.provider_type_id.name or '',
-                        'payment_method': '',
-                        'NTA_amount_real': self.total_nta and self.total_nta or 0,
-                        'payment_acquirer': pay_acq and pay_acq.jasaweb_name or ''
-                    })
-                    rec.sudo().write({
-                        'is_sent_to_acc': True
-                    })
+                        ledger_list.append({
+                            'reference_number': rec.ref and rec.ref or '',
+                            'name': rec.name and rec.name or '',
+                            'debit': rec.debit and rec.debit or 0,
+                            'credit': rec.credit and rec.credit or 0,
+                            'currency_id': rec.currency_id and rec.currency_id.name or '',
+                            'create_date': rec.create_date and datetime.strftime(rec.create_date, '%Y-%m-%d %H:%M:%S') or '',
+                            'date': rec.date and datetime.strftime(rec.date, '%Y-%m-%d') or '',
+                            'create_uid': rec.create_uid and rec.create_uid.name or '',
+                            'commission': 0.0,
+                            'description': rec.description and rec.description or '',
+                            'agent_id': rec.agent_id and rec.agent_id.name,
+                            'company_sender': rec.agent_id and rec.agent_id.name,
+                            'company_receiver': self.env.ref('tt_base.rodex_ho').name,
+                            'state': 'Done',
+                            'display_provider_name': rec.display_provider_name and rec.display_provider_name or '',
+                            'pnr': rec.pnr and rec.pnr or '',
+                            'url_legacy': base_url + '/web#id=' + str(rec.id) + '&model=tt.ledger&view_type=form',
+                            'transaction_type': trans_type,
+                            'transport_type': self.provider_type_id and self.provider_type_id.name or '',
+                            'payment_method': '',
+                            'NTA_amount_real': self.total_nta and self.total_nta or 0,
+                            'payment_acquirer': pay_acq and pay_acq.jasaweb_name or ''
+                        })
+                        rec.sudo().write({
+                            'is_sent_to_acc': True
+                        })
             if ledger_list:
                 new_obj = self.env['tt.accounting.queue'].create({
                     'request': json.dumps(ledger_list),
