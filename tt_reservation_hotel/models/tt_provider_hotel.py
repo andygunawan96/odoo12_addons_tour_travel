66fc12288b9012153b1300a2c73eafb88f2de60f
Joshua <it@rodextravel.tours>
2022-03-22 15:53:22 +0700

    ledger created reverse ledger hotel

--- tt_reservation_hotel/models/tt_provider_hotel.py
+++ tt_reservation_hotel/models/tt_provider_hotel.py
@@ -68,6 +68,9 @@ class TransportBookingProvider(models.Model):
             if rec.pnr in [self.pnr, str(self.sequence)] and not rec.is_reversed:
                 rec.reverse_ledger()
 
+        for rec in self.cost_service_charge_ids:
+            rec.is_ledger_created = False
+
         self.write({
             'state': 'fail_refunded',
             # 'is_ledger_created': False,
