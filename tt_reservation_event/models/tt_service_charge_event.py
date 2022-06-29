75601c25ffe5981608ac3e6c4c21f0c319259d0c
Andy Gunawan <it@rodextravel.tours>
2022-06-29 10:53:29 +0700

    fix_event_flow_and_ticket

--- tt_reservation_event/models/tt_service_charge_event.py
+++ tt_reservation_event/models/tt_service_charge_event.py
@@ -6,6 +6,9 @@ class TtServiceChargeEvent(models.Model):
 
     provider_event_booking_id = fields.Many2one('tt.provider.event', 'Provider ID')
     booking_event_id = fields.Many2one('tt.reservation.event', 'Booking id', ondelete="cascade", index=True, copy=False)
+    passenger_event_ids = fields.Many2many('tt.reservation.passenger.event',
+                                             'tt_reservation_event_cost_charge_rel', 'service_charge_id',
+                                             'passenger_id', 'Passenger Event')
     # passenger_event_ids = fields.Many2many('tt.reservation.event.option', 'tt_reservation_event_cost_charge_rel', 'service_charge_id', 'option_id', 'Event Option')
     # passenger_event_ids = fields.Many2many('tt.reservation.passenger.event', 'tt_reservation_event_cost_charge_rel', 'service_charge_id', 'option_id', 'Event Option')
     # option_event_ids = fields.Many2many('tt.reservation.event.option', 'tt_reservation_event_cost_charge_rel', 'service_charge_id', 'option_id', 'Event Option')
