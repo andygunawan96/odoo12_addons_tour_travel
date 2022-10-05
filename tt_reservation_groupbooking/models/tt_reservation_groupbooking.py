b5458c284e6354b15f487ad6f8924a3525982b4a
ivan <ivan@rodextravel.tours>
2022-10-05 16:55:51 +0700

    testing_quota_pnr

--- tt_reservation_groupbooking/models/tt_reservation_groupbooking.py
+++ tt_reservation_groupbooking/models/tt_reservation_groupbooking.py
@@ -2048,8 +2048,15 @@ class ReservationGroupBooking(models.Model):
             raise RequestException(1008)
         booker_obj = self.create_booker_api(data['booker'], context)  # create booker
         if book_obj.contact_id:
-            data_contact = {
-                "title": book_obj.contact_id.title,
+            title = ''
+            if book_obj.contact_id.gender == 'male':
+                title = 'MR'
+            elif book_obj.contact_id.gender == 'female' and book_obj.contact_id.marital_status in ['', 'single']:
+                title = 'MS'
+            else:
+                title = 'MRS'
+            data['contacts'] = [{
+                "title": title,
                 "first_name": book_obj.contact_id.first_name,
                 "last_name": book_obj.contact_id.last_name,
                 "email": book_obj.contact_id.email,
@@ -2057,9 +2064,10 @@ class ReservationGroupBooking(models.Model):
                 "mobile": book_obj.contact_id.phone_ids[0].calling_number,
                 "contact_seq_id": book_obj.contact_id.seq_id,
                 "is_also_booker": False,
-                "nationality_code": book_obj.contact_id.nationality_id.code
-            }
-            contact_obj = self.create_contact_api(data_contact, booker_obj, context)
+                "nationality_code": book_obj.contact_id.nationality_id.code,
+                "gender": book_obj.contact_id.gender
+            }]
+            contact_obj = self.create_contact_api(data['contacts'][0], booker_obj, context)
             book_obj.update({
                 'contact_id': contact_obj.id,
                 'contact_title': data['contacts'][0]['title'],
