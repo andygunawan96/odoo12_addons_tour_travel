d20e6c0678d118543b37ae5326564d0a57366b65
ivan <ivan@rodextravel.tours>
2022-10-05 13:07:42 +0700

    testing_quota_pnr

--- tt_reservation/models/tt_reservation.py
+++ tt_reservation/models/tt_reservation.py
@@ -1025,6 +1025,7 @@ class TtReservation(models.Model):
             else:
                 book_obj = self.env['tt.reservation.%s' % (req['table_name'])].search([('name', '=', req.get('order_number'))],
                                                                                limit=1)
+            _logger.error(json.dumps(req))
             if req.get('is_sync_reservation'):
                 if not req['is_sync_reservation']:
                     _logger.error('UPDATE SYNC RESV FALSE HERE')
