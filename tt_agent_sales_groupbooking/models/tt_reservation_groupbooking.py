b827644358b0eb57b6703a857e03f35d09b8b6a3
Andy Gunawan <it@rodextravel.tours>
2022-11-10 17:10:15 +0700

    fix_invoice_grand_total

--- tt_agent_sales_groupbooking/models/tt_reservation_groupbooking.py
+++ tt_agent_sales_groupbooking/models/tt_reservation_groupbooking.py
@@ -193,7 +193,7 @@ class ReservationGroupBooking(models.Model):
             payment_obj = self.env['tt.payment'].create({
                 'agent_id': self.agent_id.id,
                 'acquirer_id': acquirer_id,
-                'real_total_amount': inv_line_obj.total_after_tax,
+                'real_total_amount': invoice_id.grand_total,
                 'customer_parent_id': customer_parent_id,
                 'confirm_uid': co_uid,
                 'confirm_date': datetime.now()
