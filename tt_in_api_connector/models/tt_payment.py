b17c57b8e5ed23ad6ea26c4384c36a41027078b2
ivan <ivan@rodextravel.tours>
2022-11-23 09:51:24 +0700

    update_payment_espay_waiting

--- tt_in_api_connector/models/tt_payment.py
+++ tt_in_api_connector/models/tt_payment.py
@@ -100,7 +100,7 @@ class TtPaymentApiCon(models.Model):
                     book_obj = self.env['tt.reservation.%s' % data['provider_type']].search([('name', '=', data['order_number']), ('state', 'in', ['booked','halt_booked'])], limit=1)
                     _logger.info(data['order_number'])
                     if book_obj:
-                        pay_acq_num = self.env['payment.acquirer.number'].search([('number', 'ilike', data['order_number']), ('state', 'in', ['close','done'])],limit=1) ## SELECT ULANG KARENA BISA CONCURRENT, UNTUK AMBIL FEE AMOUNT
+                        pay_acq_num = self.env['payment.acquirer.number'].search([('number', 'ilike', data['order_number']), ('state', 'in', ['close','waiting','done'])],limit=1) ## SELECT ULANG KARENA BISA CONCURRENT, UNTUK AMBIL FEE AMOUNT
                         reservation_transaction_amount = book_obj.total - book_obj.total_discount
                         if pay_acq_num:
                             reservation_transaction_amount += pay_acq_num.fee_amount
