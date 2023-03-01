70aa6193a0cb3163ca18c9921f3ffdc83a598252
ivan <ivan@rodextravel.tours>
2023-03-01 13:31:00 +0700

    update_behaviors

--- tt_reservation/models/tt_reservation.py
+++ tt_reservation/models/tt_reservation.py
@@ -499,6 +499,11 @@ class TtReservation(models.Model):
                     if vals_for_update:
                         current_passenger.update(vals_for_update)
 
+                    ## update customer behavior 1 maret 2023 IVAN
+                    if psg.get('behaviors'):
+                        for provider_type in psg['behaviors']:
+                            current_passenger.add_behavior(provider_type.lower(), psg['behaviors'][provider_type])
+
                     if psg.get('identity'):
                         current_passenger.add_or_update_identity(psg['identity'])
                     if psg.get('ff_numbers'):
