03e8aec89289c4dfb9617b0a3319bfab0ac979d9
joshua.laptop <joshuamail.laptop>
2023-03-20 20:36:06 +0700

    changing agent balance log order

--- tt_agent_report_balance/models/tt_agent_report_balance_log.py
+++ tt_agent_report_balance/models/tt_agent_report_balance_log.py
@@ -3,6 +3,7 @@ from odoo import models, fields, api
 class TtAgentReportBalanceLog(models.Model):
     _name = 'tt.agent.report.balance.log'
     _description = 'Agent Report Balance Log'
+    _order = 'id desc'
 
     name = fields.Char(string='File Name',default='agent_balance_report_log.xls')
     file = fields.Binary(string='File')
