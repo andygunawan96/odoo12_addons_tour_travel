c4262a50c7dea76a91b2412652ae7bd223418541
Andy Gunawan <it@rodextravel.tours>
2023-08-23 11:19:32 +0700

    agent_balance_report_credit_limit

--- tt_base/wizard/move_agent_ho_wizard.py
+++ tt_base/wizard/move_agent_ho_wizard.py
@@ -1,5 +1,8 @@
 from odoo import api, fields, models, _
 from datetime import datetime
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class MoveAgentHOWizard(models.TransientModel):
@@ -11,12 +14,14 @@ class MoveAgentHOWizard(models.TransientModel):
     new_ho_id = fields.Many2one('tt.agent', 'New Head Office', domain=[('is_ho_agent', '=', True)], required=True)
 
     def submit_move_ho(self):
-        if self.new_ho_id.id != self.old_ho_id.id:
-            child_agent_list = self.env['tt.agent'].search([('parent_agent_id', '=', self.agent_id.id)])
-            for rec in child_agent_list:
-                rec.write({
-                    'parent_agent_id': self.agent_id.parent_agent_id.id
-                })
+        old_ho_id = self.agent_id.ho_id.id
+        old_ho_name = self.agent_id.ho_id.name
+        if self.new_ho_id.id != old_ho_id:
+            _logger.info('Moving %s from %s to %s...' % (self.agent_id.name, old_ho_name, self.new_ho_id.name))
+            sql_query = """
+            update tt_agent set parent_agent_id = %s where parent_agent_id = %s;
+            """ % (self.agent_id.parent_agent_id.id, self.agent_id.id)
+            self.env.cr.execute(sql_query)
             self.agent_id.write({
                 'ho_id': self.new_ho_id.id,
                 'parent_agent_id': self.new_ho_id.id
@@ -24,48 +29,48 @@ class MoveAgentHOWizard(models.TransientModel):
             # model_list = self.env['ir.model'].search([('field_id.name', '=', 'ho_id'), ('field_id.name', 'in', ['agent_id', 'parent_agent_id'])])
             # for rec in model_list:
             #     _logger.info(rec.model)
-            cus_par_list = self.env['tt.customer.parent'].search([('parent_agent_id', '=', self.agent_id.id), ('ho_id', '=', self.old_ho_id.id)])
-            for rec in cus_par_list:
-                rec.write({
-                    'ho_id': self.new_ho_id.id
-                })
-            customer_list = self.env['tt.customer'].search([('ho_id', '=', self.old_ho_id.id), '|', ('agent_id', '=', self.agent_id.id), ('agent_as_staff_id', '=', self.agent_id.id)])
-            for rec in customer_list:
-                rec.write({
-                    'ho_id': self.new_ho_id.id
-                })
-            user_list = self.env['res.users'].search([('agent_id', '=', self.agent_id.id), ('ho_id', '=', self.old_ho_id.id)])
-            for rec in user_list:
-                rec.write({
-                    'ho_id': self.new_ho_id.id
-                })
-            payment_acq_list = self.env['payment.acquirer'].search([('agent_id', '=', self.agent_id.id), ('ho_id', '=', self.old_ho_id.id)])
-            for rec in payment_acq_list:
-                rec.write({
-                    'ho_id': self.new_ho_id.id
-                })
-            payment_acq_num_list = self.env['payment.acquirer.number'].search([('agent_id', '=', self.agent_id.id), ('ho_id', '=', self.old_ho_id.id)])
-            for rec in payment_acq_num_list:
-                rec.write({
-                    'ho_id': self.new_ho_id.id
-                })
-            phone_list = self.env['phone.detail'].search([('agent_id', '=', self.agent_id.id), ('ho_id', '=', self.old_ho_id.id)])
-            for rec in phone_list:
-                rec.write({
-                    'ho_id': self.new_ho_id.id
-                })
-            address_list = self.env['address.detail'].search([('agent_id', '=', self.agent_id.id), ('ho_id', '=', self.old_ho_id.id)])
-            for rec in address_list:
-                rec.write({
-                    'ho_id': self.new_ho_id.id
-                })
-            socmed_list = self.env['social.media.detail'].search([('agent_id', '=', self.agent_id.id), ('ho_id', '=', self.old_ho_id.id)])
-            for rec in socmed_list:
-                rec.write({
-                    'ho_id': self.new_ho_id.id
-                })
-            third_party_list = self.env['tt.agent.third.party.key'].search([('agent_id', '=', self.agent_id.id), ('ho_id', '=', self.old_ho_id.id)])
-            for rec in third_party_list:
-                rec.write({
-                    'ho_id': self.new_ho_id.id
-                })
+
+            sql_query = """
+                        update tt_customer_parent set ho_id = %s where parent_agent_id = %s and ho_id = %s;
+                        """ % (self.new_ho_id.id, self.agent_id.id, old_ho_id)
+            self.env.cr.execute(sql_query)
+
+            sql_query = """
+                        update tt_customer set ho_id = %s where (agent_id = %s or agent_as_staff_id = %s) and ho_id = %s;
+                        """ % (self.new_ho_id.id, self.agent_id.id, self.agent_id.id, old_ho_id)
+            self.env.cr.execute(sql_query)
+
+            sql_query = """
+                        update res_users set ho_id = %s where agent_id = %s and ho_id = %s;
+                        """ % (self.new_ho_id.id, self.agent_id.id, old_ho_id)
+            self.env.cr.execute(sql_query)
+
+            sql_query = """
+                        update payment_acquirer set ho_id = %s where agent_id = %s and ho_id = %s;
+                        """ % (self.new_ho_id.id, self.agent_id.id, old_ho_id)
+            self.env.cr.execute(sql_query)
+
+            sql_query = """
+                        update payment_acquirer_number set ho_id = %s where agent_id = %s and ho_id = %s;
+                        """ % (self.new_ho_id.id, self.agent_id.id, old_ho_id)
+            self.env.cr.execute(sql_query)
+
+            sql_query = """
+                        update phone_detail set ho_id = %s where agent_id = %s and ho_id = %s;
+                        """ % (self.new_ho_id.id, self.agent_id.id, old_ho_id)
+            self.env.cr.execute(sql_query)
+
+            sql_query = """
+                        update address_detail set ho_id = %s where agent_id = %s and ho_id = %s;
+                        """ % (self.new_ho_id.id, self.agent_id.id, old_ho_id)
+            self.env.cr.execute(sql_query)
+
+            sql_query = """
+                        update social_media_detail set ho_id = %s where agent_id = %s and ho_id = %s;
+                        """ % (self.new_ho_id.id, self.agent_id.id, old_ho_id)
+            self.env.cr.execute(sql_query)
+
+            sql_query = """
+                        update tt_agent_third_party_key set ho_id = %s where agent_id = %s and ho_id = %s;
+                        """ % (self.new_ho_id.id, self.agent_id.id, old_ho_id)
+            self.env.cr.execute(sql_query)
