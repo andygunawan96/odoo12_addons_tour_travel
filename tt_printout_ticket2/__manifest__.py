b4c431d162443f22808a5d1e66de8bf5eea5572a
vin <vincent@rodextravel.tours>
2023-10-11 08:29:44 +0700

    report templte 2

---
+++ tt_printout_ticket2/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Tour & Travel - Printout Ticket 2',
+    'version': '2.0',
+    'category': 'Report',
+    'sequence': 80,
+    'summary': 'Printout Template #2 Haryono',
+    'description': """
+Tour & Travel - Printout Ticket 2
+=================================
+Key Features
+------------
+    """,
+    'author': 'PT Orbis Daya Asia',
+    'website': 'orbisway.com',
+    'depends': ['tt_report_common'],
+
+    # always loaded
+    'data': [
+        'report/printout_ticket_airline_template_2.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
