36cdd91fbeead1f9b0da0c4f98d819e1403ae9d9
Andy Gunawan <it@rodextravel.tours>
2023-11-09 14:32:04 +0700

    fix_bug

--- tt_base/models/official_odoo_inh/res_users.py
+++ tt_base/models/official_odoo_inh/res_users.py
@@ -283,7 +283,7 @@ class ResUsers(models.Model):
         if 'is_using_otp' in vals:
             if not ({self.env.ref('base.group_system').id, self.env.ref('tt_base.group_user_data_level_5').id}.intersection(set(self.env.user.groups_id.ids))):
                 vals.pop('is_using_otp')
-            if not self.env.user.has_group('base.group_system') and not vals['is_using_otp']:
+            elif not self.env.user.has_group('base.group_system') and not vals['is_using_otp']:
                 vals.pop('is_using_otp')
         if vals.get('password'):
             self._check_password(vals['password'])
