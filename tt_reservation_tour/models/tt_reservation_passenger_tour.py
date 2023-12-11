7cfb8b6df5c2a1b1cdc168ffcc2daa05336890eb
ivan <ivan@rodextravel.tours>
2023-12-11 15:13:22 +0700

    update_price_breakdown

--- tt_reservation_tour/models/tt_reservation_passenger_tour.py
+++ tt_reservation_tour/models/tt_reservation_passenger_tour.py
@@ -33,6 +33,7 @@ class TtReservationCustomer(models.Model):
             'tour_room_id': self.tour_room_id and self.tour_room_id.id or 0,
             'pax_type': self.pax_type and self.pax_type or '',
             'sale_service_charges': sale_service_charges,
+            'service_charge_details': self.get_service_charge_details()
         })
         if len(self.channel_service_charge_ids.ids)>0:
             res['channel_service_charges'] = self.get_channel_service_charges()
