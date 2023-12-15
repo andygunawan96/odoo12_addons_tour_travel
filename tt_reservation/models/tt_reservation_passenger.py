18d01e4dc7fffd54943d6d84c98648b8dd0cb074
sam <samvi@rodextravel.tours>
2023-12-15 11:46:56 +0700

    update breakdown untuk get booking (breakdown detail)

--- tt_reservation/models/tt_reservation_passenger.py
+++ tt_reservation/models/tt_reservation_passenger.py
@@ -184,7 +184,8 @@ class TtReservationCustomer(models.Model):
                     elif charge_type == 'RACAVP':
                         base_commission_provider_vat += sc_amount
                     elif charge_type == 'ROCHSP':
-                        base_fee_ho += sc_amount
+                        # base_fee_ho += sc_amount
+                        pass
                     elif charge_type == 'ROCHVP':
                         base_vat_ho += sc_amount
                     elif charge_type == 'ROCAVP':
@@ -211,7 +212,9 @@ class TtReservationCustomer(models.Model):
             base_price = base_fare + base_tax + base_roc + base_discount
             base_commission_airline = base_commission + base_commission_upline + base_commission_provider_ho + base_commission_provider_vat + base_commission_charge
             base_nta = base_price + base_commission
-            base_nta_airline = base_price_ori + base_commission_airline
+            # base_nta_airline = base_price_ori + base_commission_airline
+            base_convenience_fee = base_price - base_price_ori - base_fee_ho - base_vat_ho
+            base_nta_airline = base_price_ori + base_commission_airline + base_convenience_fee
 
             pax_values = {
                 'pnr': pnr,
@@ -220,6 +223,7 @@ class TtReservationCustomer(models.Model):
                 'pax_count': 1,
                 'base_fare': float(base_fare),
                 'base_tax': float(base_tax_total),
+                'base_convenience_fee': float(base_convenience_fee),
                 'base_discount': float(base_discount),
                 'base_fare_ori': float(base_fare),
                 'base_tax_ori': float(base_tax),
